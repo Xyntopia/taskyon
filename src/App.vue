b0c9dda838e833ec13e2c63fa027aab72b05e10c
yeus <Thomas.Meschede@web.de>
2023-09-12 19:31:21 -0700

    and more refactoring! :)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <router-view />
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'App',
-  setup( /*props*/ ) {
-    /*const $q = useQuasar()
+<script setup lang="ts">
 
-    $q.iconMapFn = (iconName) => {
-    const icon = myIcons[iconName]
-    if (icon !== void 0) {
-      return { icon: icon }
-    }*/
-  
-  }
-})
+/*const $q = useQuasar()
+
+$q.iconMapFn = (iconName) => {
+const icon = myIcons[iconName]
+if (icon !== void 0) {
+  return { icon: icon }
+}*/
 </script>
--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -22,10 +22,5 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'Error404'
-})
+<script setup lang="ts">
 </script>
