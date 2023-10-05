1a58e19f2ef9718edfb19f050d29a3221cea07c7
yeus <Thomas.Meschede@web.de>
2023-10-04 22:38:04 -0700

    enabling auto dark mode

--- quasar.conf.js
+++ quasar.conf.js
@@ -248,7 +248,7 @@ module.exports = configure(function (ctx) {
       iconSet: 'material-icons', // Quasar icon set
       lang: 'en-US', // Quasar language pack
       config: {
-        dark: 'false', // 'auto' or Boolean true/false
+        dark: 'auto', // 'auto' or Boolean true/false
         // https://coolors.co/003459-c46e72-820263-061623-618b25-e43a64-b0bbbf-e6af2e
         // TODO: these colors are currently defined in ./css/quasar.variables.sass
         /* brand: {
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,15 +2,15 @@
   <div class="fit column">
     <div :class="[theme, 'full-height', 'full-width', 'q-pa-md']">
       <!-- Settings Area -->
-      <q-expansion-item label="Settings" icon="settings">
+      <q-expansion-item dense label="Settings" icon="settings">
         <q-input v-model="openAIKey" label="OpenAI Key" />
         <q-toggle v-model="isDarkTheme" label="Dark Theme" />
       </q-expansion-item>
 
       <q-card-section class="row items-center">
         <div class="shadow-2 bg-grey-3 text-white rounded-borders">
-          <div v-for="message in messages" :key="message.id"
-            :class="message.type === 'bot' ? 'message bot-message bg-primary' : 'message user-message bg-secondary'">
+          <div v-for="message, idx in messages" :key="idx"
+            :class="message.role === 'assistant' ? 'message bot-message bg-primary' : 'message user-message bg-secondary'">
             {{ message.content }}
           </div>
         </div>
@@ -25,24 +25,12 @@
 </template>
 
 
-<style lang="sass" scoped>
-$dark-theme-color: $primary
-$light-theme-color: white
-
-.dark-theme
-  background-color: $dark-theme-color
-  color: #fff
-
-.light-theme
-  background-color: $light-theme-color
-  color: #333
-</style>
-
-
-
 <script setup lang="ts">
 import { ref, watch } from 'vue';
+import { useQuasar } from 'quasar';
 import axios from 'axios';
+
+const $q = useQuasar();
 
 type OpenAIResponse = {
   id: string,
@@ -66,24 +54,26 @@ type OpenAIResponse = {
   }
 };
 
+type OpenAIMessage = {
+  role: string,
+  content: string
+}
 
+const messages = ref<OpenAIMessage[]>([]);
+const userInput = ref<string>('');
 
-const callOpenAI = async (userMessage: string): Promise<string> => {
+const callOpenAI = async (userMessage: OpenAIMessage): Promise<string> => {
   const apiKey = localStorage.getItem('openai_key'); // Fetching from local storage
 
   if (!apiKey) {
     throw new Error('API Key not found in local storage');
   }
 
-  // Constructing the payload based on OpenAI chat endpoint structure
+  // Prepare the payload, including all the messages from the chat history
   const payload = {
     model: 'gpt-3.5-turbo',
-    messages: [
-      ...messages.value,
-      { role: 'user', content: userMessage }
-    ]
+    messages: messages.value
   };
-
 
   const response = await axios.post<OpenAIResponse>('https://api.openai.com/v1/chat/completions', payload, {
     headers: {
@@ -92,38 +82,34 @@ const callOpenAI = async (userMessage: string): Promise<string> => {
     }
   });
 
-
   // Extracting the bot's message from the response
-  return response.data?.choices[0]?.message?.content ?? '';
-};
+  const botResponseContent = response.data?.choices[0]?.message?.content ?? '';
 
-const messages = ref<Array<{ id: number, type: 'user' | 'bot', content: string }>>([]);
-const userInput = ref<string>('');
+  // Add the bot's response to the existing messages array
+  messages.value.push({ role: 'assistant', content: botResponseContent });
+
+  return botResponseContent;
+};
 
 const sendMessage = async () => {
   if (userInput.value.trim() === '') return;
 
   const userMessage = {
-    id: Date.now(),
-    type: 'user',
+    role: 'user',
     content: userInput.value
   };
 
-  messages.value.push(userMessage);
+  messages.value.push(userMessage);  // Here
 
   // Getting bot's response and pushing it to messages array
-  const botResponseContent = await callOpenAI(userInput.value);
-  messages.value.push({ id: Date.now() + 1, type: 'bot', content: botResponseContent });
-
-  userInput.value = '';
+  const botResponseContent = await callOpenAI(userMessage);
 };
 
 const openAIKey = ref<string>(localStorage.getItem('openai_key') || '');
-const isDarkTheme = ref<boolean>(false);
-const theme = ref<string>('light-theme');
+const isDarkTheme = ref<boolean>($q.dark.isActive);
 
 watch(isDarkTheme, (newValue) => {
-  theme.value = newValue ? 'dark-theme' : 'light-theme';
+  $q.dark.set(newValue);
 });
 
 watch(openAIKey, (newValue) => {
