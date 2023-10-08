22fe3db9bd66e6b0d715a6ba346f6f1ede0436b8
yeus <Thomas.Meschede@web.de>
2023-10-07 23:28:39 -0700

    adding more docs

--- src/modules/saveState.ts
+++ src/modules/saveState.ts
@@ -1,21 +1,42 @@
 import { watch, ref, UnwrapRef, Ref } from 'vue';
 
 // localStorage.ts
+
+/**
+ * Loads a state from localStorage using a specified key.
+ *
+ * @template T - The type of the state.
+ * @param {string} key - The key under which the state is stored in localStorage.
+ * @returns {T | null} - The state, parsed from JSON, or null if no state was found.
+ */
 function loadState<T>(key: string): T | null {
   const raw = localStorage.getItem(key);
   return raw ? (JSON.parse(raw) as T) : null;
 }
 
+/**
+ * Saves a state to localStorage under a specified key.
+ *
+ * @template T - The type of the state.
+ * @param {string} key - The key under which to store the state in localStorage.
+ * @param {T} state - The state to store.
+ */
 function saveState<T>(key: string, state: T): void {
   localStorage.setItem(key, JSON.stringify(state));
 }
 
-// make a state reactive and automatically save & load it from localstorage
+/**
+ * Makes a state reactive and automatically saves and loads it from localStorage.
+ *
+ * @template T - The type of the state.
+ * @param {string} key - The key under which to store and retrieve the state in localStorage.
+ * @param {T} initialState - The initial state, to be used if no state is currently stored in localStorage.
+ * @returns {Ref<UnwrapRef<T>>} - A reactive reference to the state.
+ */
 export function syncStateWLocalStorage<T>(
   key: string,
   initialState: T
 ): Ref<UnwrapRef<T>> {
-  console.log('load!!!')
   const savedState = loadState<T>(key);
   const state = ref(initialState);
   if (savedState != null) {
