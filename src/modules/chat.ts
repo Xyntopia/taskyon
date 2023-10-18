49b8755e9f516e858f5387ddd9b0447ae6939764
yeus <Thomas.Meschede@web.de>
2023-10-17 22:45:45 -0700

    adding async queue

--- src/modules/chat.ts
+++ src/modules/chat.ts
@@ -329,6 +329,38 @@ async function generateContext(
   }
 }
 
+class AsyncQueue {
+  private queue: (() => Promise<void>)[] = [];
+  private processing = false;
+
+  async enqueue(task: () => Promise<void>): Promise<void> {
+    this.queue.push(task);
+    if (!this.processing) {
+      this.processing = true;
+      await this.processNext();
+    }
+  }
+
+  private async processNext(): Promise<void> {
+    if (this.queue.length === 0) {
+      this.processing = false;
+      return;
+    }
+
+    const task = this.queue.shift();
+    if (task) {
+      try {
+        await task();
+      } catch (error) {
+        console.error('Error processing task:', error);
+      }
+
+      // Process the next task in the queue
+      await this.processNext();
+    }
+  }
+}
+
 export const sendMessage = async (message: string) => {
   const conversation = activeConversation();
   if (message.trim() === '' || !conversation) return;
