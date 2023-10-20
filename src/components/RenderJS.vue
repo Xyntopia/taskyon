803e1f84b5ab872643687052638b65830425e02b
yeus <Thomas.Meschede@web.de>
2023-10-20 12:59:22 -0700

    updated renderjs without pulling in new libraries

--- src/components/RenderJS.vue
+++ src/components/RenderJS.vue
@@ -1,38 +1,31 @@
 <template>
-    <div>
-      <iframe ref="sandbox" :srcdoc="sanitizedContent" frameborder="0"></iframe>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted, watch } from 'vue';
-  import DOMPurify from 'dompurify';
-  
-  // Prop to accept the HTML + JavaScript string
-  const content = ref('<h1>Hello World</h1><script>alert("Hello World");</script>');
-  
-  const sanitizedContent = ref('');
-  
-  // Watch for changes to content and sanitize it
-  watch(content, (newContent) => {
-    sanitizedContent.value = DOMPurify.sanitize(newContent, { ADD_TAGS: ['script'] });
-  });
-  
-  const sandbox = ref(null);
-  
-  onMounted(() => {
-    // Ensure iframe is sandboxed, but allow scripts
+  <div>
+    <iframe ref="sandbox" :srcdoc="content" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+// Prop to accept the HTML + JavaScript string
+const content = ref<string>('<h1>Hello World</h1>' + '<script>alert("Hello World");<\/script>');
+
+const sandbox = ref<HTMLIFrameElement | null>(null);
+
+onMounted(() => {
+  // Ensure iframe is sandboxed, but allow scripts
+  if (sandbox.value) {
     sandbox.value.setAttribute('sandbox', 'allow-scripts');
-  });
-  
-  </script>
-  
-  <style scoped>
-  /* Scoped styles for this component */
-  iframe {
-    width: 100%;
-    height: 100%;
-    border: none;
   }
-  </style>
-  
+});
+</script>
+
+<style scoped>
+/* Scoped styles for this component */
+iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
+
