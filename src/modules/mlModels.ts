47e13039b6d7e543d612f9a2ed697a2e2eb35863
yeus <Thomas.Meschede@web.de>
2023-10-20 11:42:41 -0700

    add potential loading of vector dictionary

--- src/modules/mlModels.ts
+++ src/modules/mlModels.ts
@@ -8,6 +8,20 @@ import {
 } from '@xenova/transformers';
 import { ref } from 'vue';
 
+// Include pako library
+// this piece of code loads a compressed vocabulary for vectorization tasks...
+/*import pako from 'pako';
+
+fetch('compressed_array.b64')
+    .then(response => response.text())
+    .then(data => {
+        let binaryData = atob(data);
+        let compressedData = new Uint8Array(binaryData.split("").map(char => char.charCodeAt(0)));
+        let decompressedData = pako.inflate(compressedData);
+        let myArray = new Float32Array(decompressedData.buffer);
+    });
+*/
+
 const state = ref({
   models: {} as Record<string, PreTrainedModel>,
   tokenizers: {} as Record<string, PreTrainedTokenizer>,
