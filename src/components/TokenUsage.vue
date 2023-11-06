41af9d2f28606aa4d4b84bc4159e0e1def4bd08c
yeus <Thomas.Meschede@web.de>
2023-11-06 14:40:01 -0800

    more reractoring...

--- src/components/TokenUsage.vue
+++ src/components/TokenUsage.vue
@@ -8,15 +8,15 @@
       style="background-color: inherit; color: inherit"
     >
       Token Usage
-      <tbody v-if="message.debugging?.aiResponse?.usage">
+      <tbody v-if="task.debugging?.aiResponse?.usage">
         <tr>
           <td class="text-left">Prompt (estimated Ts for functions):</td>
           <td class="text-right">
             +
-            {{ message.debugging?.aiResponse?.usage.prompt_tokens }}
+            {{ task.debugging?.aiResponse?.usage.prompt_tokens }}
             (
             {{
-              estimateChatTokens(message, state.chatState).functionTokens * 0.8
+              estimateChatTokens(task, state.chatState).functionTokens * 0.8
             }})
           </td>
         </tr>
@@ -24,27 +24,27 @@
           <td class="text-left">Completion:</td>
           <td class="text-right">
             +
-            {{ message.debugging?.aiResponse?.usage.completion_tokens }}
+            {{ task.debugging?.aiResponse?.usage.completion_tokens }}
           </td>
         </tr>
         <tr>
           <td class="text-left">Total:</td>
           <td class="text-right">
             =
-            {{ message.debugging?.aiResponse?.usage.total_tokens }} (exact)
+            {{ task.debugging?.aiResponse?.usage.total_tokens }} (exact)
           </td>
         </tr>
-        <tr v-if="message.debugging?.inference_costs">
+        <tr v-if="task.debugging?.inference_costs">
           <td class="text-left">Costs:</td>
           <td class="text-right">
             =
             {{
               Math.round(
-                message.debugging?.inference_costs * 1e6
+                task.debugging?.inference_costs * 1e6
               ).toLocaleString()
             }}
             μ$ (exact, ={{
-              Math.round(0.01 / message.debugging?.inference_costs)
+              Math.round(0.01 / task.debugging?.inference_costs)
             }}
             messages to reach $0.01)
           </td>
@@ -55,31 +55,31 @@
           <td class="text-left">Functions (estimated):</td>
           <td class="text-right">
             +
-            {{ estimateChatTokens(message, state.chatState).functionTokens }}
+            {{ estimateChatTokens(task, state.chatState).functionTokens }}
           </td>
         </tr>
         <tr>
           <td class="text-left">Prompt (estimated):</td>
           <td class="text-right">
             +
-            {{ estimateChatTokens(message, state.chatState).promptTokens }}
+            {{ estimateChatTokens(task, state.chatState).promptTokens }}
           </td>
         </tr>
         <tr>
           <td class="text-left">Conversation (estimated):</td>
           <td class="text-right">
             +
-            {{ estimateChatTokens(message, state.chatState).chatTokens }}
+            {{ estimateChatTokens(task, state.chatState).chatTokens }}
           </td>
         </tr>
         <tr>
           <td class="text-left">Total:</td>
-          <td v-if="message.debugging?.usedTokens" class="text-right">
-            = {{ message.debugging?.usedTokens }} (exact)
+          <td v-if="task.debugging?.usedTokens" class="text-right">
+            = {{ task.debugging?.usedTokens }} (exact)
           </td>
           <td v-else class="text-right">
             =
-            {{ estimateChatTokens(message, state.chatState).total }}
+            {{ estimateChatTokens(task, state.chatState).total }}
             (estimated)
           </td>
         </tr>
@@ -96,7 +96,7 @@ import '@quasar/quasar-ui-qmarkdown/dist/index.css';
 import { useTaskyonStore } from 'stores/taskyonState';
 
 defineProps({
-  message: {
+  task: {
     type: Object as PropType<LLMTask>,
     required: true,
   },
