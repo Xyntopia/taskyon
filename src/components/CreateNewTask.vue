bd874fe5e994057184cca80c79cd50a4e59d3315
yeus <Thomas.Meschede@web.de>
2023-11-07 13:05:54 -0800

    refactor

--- src/components/CreateNewTask.vue
+++ src/components/CreateNewTask.vue
@@ -111,65 +111,9 @@
           </div>
         </q-item-section>
       </q-item>
-      <!--Model Selection-->
       <q-item>
         <q-item-section>
-          <q-select
-            v-if="state.chatState.baseURL == getBackendUrls('openai')"
-            class="q-pt-xs"
-            filled
-            bottom-slots
-            dense
-            label="Select Openrouter.ai LLM"
-            icon="smart_toy"
-            :options="modelOptions.openrouter"
-            emit-value
-            v-model="state.chatState.openAIModel"
-          >
-            <template v-slot:hint>
-              For a list of supported models go here:
-              <a href="https://platform.openai.com/docs/models" target="_blank"
-                >https://platform.openai.com/docs/models</a
-              >
-            </template>
-          </q-select>
-          <q-select
-            v-else
-            class="q-pt-xs"
-            filled
-            dense
-            bottom-slots
-            label="Select LLM Model for answering/solving the task."
-            icon="smart_toy"
-            :options="modelOptions.openai"
-            emit-value
-            v-model="state.chatState.openrouterAIModel"
-          >
-            <template v-slot:hint>
-              For a list of supported models go here:
-              <a href="https://openrouter.ai/docs#models" target="_blank"
-                >https://openrouter.ai/docs#models</a
-              >
-            </template>
-            <template v-slot:after>
-              <div style="font-size: 0.5em">
-                <div>
-                  prompt:
-                  {{
-                    modelLookUp.openrouter[state.chatState.openrouterAIModel]
-                      ?.pricing?.prompt
-                  }}
-                </div>
-                <div>
-                  completion:
-                  {{
-                    modelLookUp.openrouter[state.chatState.openrouterAIModel]
-                      ?.pricing?.completion
-                  }}
-                </div>
-              </div>
-            </template>
-          </q-select>
+          <ModelSelection></ModelSelection>
         </q-item-section>
       </q-item>
     </q-list>
@@ -177,29 +121,14 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch, watchEffect } from 'vue';
-import {
-  sendMessage,
-  availableModels,
-  Model,
-  getBackendUrls,
-  countStringTokens,
-  addTask2Tree,
-} from 'src/modules/chat';
-import {
-  tools,
-  FunctionArguments,
-  FunctionCall,
-  getDefaultParametersForTool,
-} from 'src/modules/tools';
+import { computed, ref, watchEffect } from 'vue';
+import { sendMessage, countStringTokens, addTask2Tree } from 'src/modules/chat';
+import { tools, getDefaultParametersForTool } from 'src/modules/tools';
 import '@quasar/quasar-ui-qmarkdown/dist/index.css';
-import openrouterModules from 'assets/openrouter_models.json';
 import { useTaskyonStore } from 'stores/taskyonState';
-import openaiModels from 'assets/openai_models.json';
 import { LLMTask } from 'src/modules/types';
+import ModelSelection from 'components/ModelSelection.vue';
 
-const openrouterModels: Model[] = openrouterModules.data;
-const openAiModels: Model[] = openaiModels.data;
 const state = useTaskyonStore();
 
 const taskTypeSelection = ref<string | undefined>(undefined);
@@ -226,65 +155,6 @@ watchEffect(() => {
     state.taskDraft.role = 'user';
   }
 });
-
-const resOpenRouter = ref<Model[]>([]);
-const resOpenAI = ref<Model[]>([]);
-async function fetchModels(): Promise<void> {
-  try {
-    resOpenRouter.value = await availableModels(
-      getBackendUrls('openrouter'),
-      state.chatState.openRouterAIApiKey
-    );
-  } catch (error) {
-    console.error('Error fetching models:', error);
-    console.log('using default list');
-    resOpenRouter.value = openrouterModels;
-  }
-  try {
-    resOpenAI.value = await availableModels(
-      getBackendUrls('openai'),
-      state.chatState.openAIApiKey
-    );
-  } catch (error) {
-    console.error('Error fetching models:', error);
-    resOpenAI.value = openAiModels;
-  }
-}
-
-const modelOptions = computed(() => ({
-  openai: resOpenRouter.value
-    .map((m) => {
-      const p = parseFloat(m.pricing?.prompt || '');
-      const c = parseFloat(m.pricing?.completion || '');
-      return { m, p: p + c };
-    })
-    .sort(({ p: p1 }, { p: p2 }) => p1 - p2)
-    .map(({ m }) => ({
-      label: `${m.id}: ${m.pricing?.prompt || 'N/A'}/${
-        m.pricing?.completion || 'N/A'
-      }`,
-      value: m.id,
-    })),
-  openrouter: [...resOpenAI.value]
-    .sort((m1, m2) => m1.id.localeCompare(m2.id))
-    .map((m) => ({
-      label: `${m.id}`,
-      value: m.id,
-    })),
-}));
-
-const modelLookUp = computed(() => ({
-  openai: resOpenAI.value.reduce((acc, m) => {
-    acc[m.id] = m;
-    return acc;
-  }, {} as Record<string, Model>),
-  openrouter: resOpenRouter.value.reduce((acc, m) => {
-    acc[m.id] = m;
-    return acc;
-  }, {} as Record<string, Model>),
-}));
-
-void fetchModels();
 
 const estimatedTokens = computed(() => {
   // Tokenize the message
@@ -323,7 +193,7 @@ const currentFunctionTask = computed(() => {
 
 function executeTask() {
   console.log('executing task!');
-  const funcTaskid = addTask2Tree(
+  addTask2Tree(
     currentFunctionTask.value,
     state.chatState.Tasks[state.chatState.selectedTaskId || ''],
     state.chatState,
---
+++ src/components/ModelSelection.vue
@@ -0,0 +1,130 @@
+<template>
+  <q-select
+    v-if="state.chatState.baseURL == getBackendUrls('openai')"
+    class="q-pt-xs"
+    filled
+    bottom-slots
+    dense
+    label="Select Openrouter.ai LLM"
+    icon="smart_toy"
+    :options="modelOptions.openrouter"
+    emit-value
+    v-model="state.chatState.openAIModel"
+  >
+    <template v-slot:hint>
+      For a list of supported models go here:
+      <a href="https://platform.openai.com/docs/models" target="_blank"
+        >https://platform.openai.com/docs/models</a
+      >
+    </template>
+  </q-select>
+  <q-select
+    v-else
+    class="q-pt-xs"
+    filled
+    dense
+    bottom-slots
+    label="Select LLM Model for answering/solving the task."
+    icon="smart_toy"
+    :options="modelOptions.openai"
+    emit-value
+    v-model="state.chatState.openrouterAIModel"
+  >
+    <template v-slot:hint>
+      For a list of supported models go here:
+      <a href="https://openrouter.ai/docs#models" target="_blank"
+        >https://openrouter.ai/docs#models</a
+      >
+    </template>
+    <template v-slot:after>
+      <div style="font-size: 0.5em">
+        <div>
+          prompt:
+          {{
+            modelLookUp.openrouter[state.chatState.openrouterAIModel]?.pricing
+              ?.prompt
+          }}
+        </div>
+        <div>
+          completion:
+          {{
+            modelLookUp.openrouter[state.chatState.openrouterAIModel]?.pricing
+              ?.completion
+          }}
+        </div>
+      </div>
+    </template>
+  </q-select>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { availableModels, Model, getBackendUrls } from 'src/modules/chat';
+import '@quasar/quasar-ui-qmarkdown/dist/index.css';
+import openrouterModules from 'assets/openrouter_models.json';
+import { useTaskyonStore } from 'stores/taskyonState';
+import openaiModels from 'assets/openai_models.json';
+
+const openrouterModels: Model[] = openrouterModules.data;
+const openAiModels: Model[] = openaiModels.data;
+const state = useTaskyonStore();
+
+const resOpenRouter = ref<Model[]>([]);
+const resOpenAI = ref<Model[]>([]);
+async function fetchModels(): Promise<void> {
+  try {
+    resOpenRouter.value = await availableModels(
+      getBackendUrls('openrouter'),
+      state.chatState.openRouterAIApiKey
+    );
+  } catch (error) {
+    console.error('Error fetching models:', error);
+    console.log('using default list');
+    resOpenRouter.value = openrouterModels;
+  }
+  try {
+    resOpenAI.value = await availableModels(
+      getBackendUrls('openai'),
+      state.chatState.openAIApiKey
+    );
+  } catch (error) {
+    console.error('Error fetching models:', error);
+    resOpenAI.value = openAiModels;
+  }
+}
+
+const modelOptions = computed(() => ({
+  openai: resOpenRouter.value
+    .map((m) => {
+      const p = parseFloat(m.pricing?.prompt || '');
+      const c = parseFloat(m.pricing?.completion || '');
+      return { m, p: p + c };
+    })
+    .sort(({ p: p1 }, { p: p2 }) => p1 - p2)
+    .map(({ m }) => ({
+      label: `${m.id}: ${m.pricing?.prompt || 'N/A'}/${
+        m.pricing?.completion || 'N/A'
+      }`,
+      value: m.id,
+    })),
+  openrouter: [...resOpenAI.value]
+    .sort((m1, m2) => m1.id.localeCompare(m2.id))
+    .map((m) => ({
+      label: `${m.id}`,
+      value: m.id,
+    })),
+}));
+
+const modelLookUp = computed(() => ({
+  openai: resOpenAI.value.reduce((acc, m) => {
+    acc[m.id] = m;
+    return acc;
+  }, {} as Record<string, Model>),
+  openrouter: resOpenRouter.value.reduce((acc, m) => {
+    acc[m.id] = m;
+    return acc;
+  }, {} as Record<string, Model>),
+}));
+
+void fetchModels();
+</script>
