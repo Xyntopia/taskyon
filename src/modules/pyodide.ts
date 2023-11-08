05ed8ad3be37969e5a30f315a307760d3440f46b
yeus <Thomas.Meschede@web.de>
2023-11-07 23:24:14 -0800

    remove some pyiodide code...

--- src/modules/pyodide.ts
+++ src/modules/pyodide.ts
@@ -56,52 +56,7 @@ export async function execute(python_script: string): Promise<PythonResult> {
   }
 }
 
-export async function main() {
-  try {
-    await loadScript(
-      'https://cdn.jsdelivr.net/pyodide/v0.24.1/full/pyodide.js'
-    );
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const pyodide = await window.loadPyodide({
-      indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.24.1/full/',
-    });
-
-    const result = pyodide.runPython('1+1');
-    console.log(result);
-  } catch (error) {
-    console.error('Loading Pyodide failed', error);
-  }
-}
 
 // you can download the releases from here:
 // url: https://github.com/pyodide/pyodide/releases
 // this gives us the chnce to package packages our own way :)
-
-// otherwise, its also possible to use this URL:
-//
-
-// export async function main() {
-//   const pyodideModule= (await import(
-//     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//   // @ts-ignore
-//     /* webpackIgnore: true */
-//     //'https://cdn.jsdelivr.net/pyodide/v0.24.1/full/pyodide.js'));
-//   /*const pyodide = await loadPyodide({
-//     indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.24.1/full/',
-//   });*/
-
-//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//   // @ts-ignore
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-//   const pyodide = await pyodideModule.loadPyodide();
-
-//   // await pyodide.loadPackage()
-//   // Pyodide is now ready to use...
-//   /*const result = pyodide.runPython(`
-//     #import sys
-//     #sys.version
-//     1+1
-//     `) as string;
-//   console.log(result);*/
-// }
