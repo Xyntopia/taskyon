f994366cfcdc14d1b61aca3d8025361ceafa3c1f
yeus <Thomas.Meschede@web.de>
2023-11-15 15:49:18 -0800

    do some modifications, code beautification

--- src/modules/rxdb.ts
+++ src/modules/rxdb.ts
@@ -1,96 +1,92 @@
 import { createRxDatabase, addRxPlugin } from 'rxdb';
-import { getRxStorageDexie } from 'rxdb/plugins/dexie';
-import { RxDBAttachmentsPlugin } from 'rxdb/plugins/attachments';
-
-// Add Dexie and Attachments plugins
-addRxPlugin(getRxStorageDexie);
-addRxPlugin(RxDBAttachmentsPlugin);
+import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
 
 async function createDatabase() {
     const db = await createRxDatabase({
-        name: 'taskyonDB',
+        name: 'taskyonDatabase',
         storage: getRxStorageDexie(),
     });
 
     // Define the schema for LLMTask
     const llmTaskSchema = {
-        title: "LLMTask schema",
+        title: 'LLMTask schema',
         version: 0,
-        type: "object",
+        type: 'object',
+        primaryKey: 'id',
         properties: {
             id: {
-                type: "string",
+                type: 'string',
                 primary: true
             },
             role: {
-                type: "string",
+                type: 'string',
                 enum: ['system', 'user', 'assistant', 'function']
             },
             content: {
-                type: ["string", "null"]
+                type: ['string', 'null']
             },
             state: {
-                type: "string",
+                type: 'string',
                 enum: ['Open', 'Queued', 'In Progress', 'Completed', 'Error']
             },
             context: {
-                type: ["object", "null"],
+                type: ['object', 'null'],
                 properties: {
                     message: {
-                        type: ["object", "null"],
+                        type: ['object', 'null'],
                         properties: {
                             // properties of OpenAIMessage
                         }
                     },
                     function: {
-                        type: ["object", "null"],
+                        type: ['object', 'null'],
                         properties: {
                             // properties of FunctionCall
                         }
                     },
                     model: {
-                        type: ["string", "null"]
+                        type: ['string', 'null']
                     },
                     uploadedFiles: {
-                        type: "array",
+                        type: 'array',
                         items: {
-                            type: "string"
+                            type: 'string'
                         }
                     }
                 }
             },
             parentID: {
-                type: ["string", "null"]
+                type: ['string', 'null']
             },
             childrenIDs: {
-                type: "array",
+                type: 'array',
                 items: {
-                    type: "string"
+                    type: 'string'
                 }
             },
             debugging: {
-                type: "object",
+                type: 'object',
                 properties: {
                     // properties of the debugging object
                 }
             },
             result: {
-                type: ["object", "null"],
+                type: ['object', 'null'],
                 properties: {
                     // properties of TaskResult
                 }
             },
             allowedTools: {
-                type: ["array", "null"],
+                type: ['array', 'null'],
                 items: {
-                    type: "string"
+                    type: 'string'
                 }
             },
             authorId: {
-                type: ["string", "null"]
+                type: ['string', 'null']
             },
             created_at: {
-                type: ["number", "null"]
+                type: ['number', 'null']
             }
         },
         required: ['id', 'role', 'state', 'childrenIDs'],
@@ -100,4 +96,39 @@ async function createDatabase() {
     };
 
     // Create the LLMTask collection
-    await
+    await db.addCollections({
+        llmtasks: {
+            schema: llmTaskSchema
+        }
+    });
+
+    return db;
+}
+
+// Use the database
+async function main() {
+    const db = await createDatabase();
+
+
+    // Example: Adding a new LLMTask
+    const llmTaskDoc = await db.llmtasks.insert({
+        id: 'task1',
+        role: 'user',
+        content: 'Some content',
+        state: 'Open',
+        childrenIDs: [],
+        // ... other properties ...
+    });
+
+    // Example: Adding an attachment to the LLMTask
+    const attachmentData = new Blob(['file data']); // Replace with actual file data
+    await llmTaskDoc.putAttachment({
+        id: 'myfile',
+        data: attachmentData,
+        type: 'text/plain'
+    });
+
+    // ... other operations ...
+}
+
+main().catch(err => console.error(err));
