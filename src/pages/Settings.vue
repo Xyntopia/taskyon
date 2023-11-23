b08245a136b2acc4fa0cadda4b36b797ddbaa215
yeus <Thomas.Meschede@web.de>
2023-11-23 00:59:37 -0800

    nice,everything works :)

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,16 +1,15 @@
 <template>
   <q-page>
     Settings
-    <ObjectTreeView v-model="chatStateProperties"/>
+    <ObjectTreeView v-model="chatStateProperties" />
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { toRefs } from 'vue';
 import { useTaskyonStore } from 'stores/taskyonState';
 import ObjectTreeView from 'components/ObjectTreeView.vue';
-import { storeToRefs } from 'pinia'
 
 const state = useTaskyonStore();
-const { Tasks, ...chatStateProperties } = storeToRefs(state).chatState.value;
+const { ...chatStateProperties } = toRefs(state.chatState);
 </script>
