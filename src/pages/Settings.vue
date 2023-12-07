8d9ba56f567e8f9d6c593b9921322af28c324f47
yeus <Thomas.Meschede@web.de>
2023-12-06 22:49:39 -0800

    download yaml/json settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,5 +1,21 @@
 <template>
   <q-page :class="$q.dark.isActive ? 'bg-primary' : 'white'">
+    <div class="q-pa-md">
+      Download Settings:
+      <q-icon name="save" size="md" />
+      <q-btn
+        label="JSON"
+        @click="downloadSettings('json')"
+        color="primary"
+        class="q-ma-md"
+      ></q-btn>
+      <q-btn
+        label="YAML"
+        @click="downloadSettings('yaml')"
+        color="primary"
+        class="q-ma-md"
+      ></q-btn>
+    </div>
     <q-tabs v-model="tab" align="justify">
       <q-tab name="settings" label="Settings" />
       <q-tab name="instructions" label="Instructions" />
@@ -24,15 +40,38 @@
 </template>
 
 <script setup lang="ts">
-import { toRefs, ref } from 'vue';
+import { exportFile, extend } from 'quasar';
+import { toRefs, ref, toRaw } from 'vue';
 import { useTaskyonStore } from 'stores/taskyonState';
 import Settings from 'components/Settings.vue';
 import ObjectTreeView from 'components/ObjectTreeView.vue';
+import yaml from 'js-yaml';
 
 const tab = ref('settings'); // Default to the first tab
 const state = useTaskyonStore();
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { chatState, ...allSettings } = state;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { Tasks, ...chatStateProperties } = toRefs(state.chatState);
+const chatStateProperties = toRefs(state.chatState);
+
+const downloadSettings = (format: string) => {
+  console.log('download settings');
+  // Convert reactive chatStateProperties to a raw object
+  const deepCopiedSettings = extend(true, {}, state.chatState);
+
+  let fileName, fileContent, mimeType;
+
+  if (format === 'json') {
+    fileName = 'taskyon_settings.json';
+    fileContent = JSON.stringify(deepCopiedSettings, null, 2);
+    mimeType = 'application/json';
+  } else {
+    fileName = 'taskyon_settings.yaml';
+    fileContent = yaml.dump(deepCopiedSettings);
+    mimeType = 'text/yaml';
+  }
+
+  // Use Quasar's exportFile function for download
+  exportFile(fileName, fileContent, mimeType);
+};
 </script>
