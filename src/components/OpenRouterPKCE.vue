bf5b0201e05205c1ec6e2b77d692ec168ce888e8
yeus <Thomas.Meschede@web.de>
2023-12-12 18:22:22 -0800

    add button openrouter.ai authentication

---
+++ src/components/OpenRouterPKCE.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-btn label="Get OpenRouter API Key" :to="authURL"></q-btn>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, computed } from 'vue';
+import { useQuasar } from 'quasar';
+import { useTaskyonStore } from 'src/stores/taskyonState';
+
+const store = useTaskyonStore();
+const $q = useQuasar();
+
+const authURL = computed(() => {
+  // TODO: get current URL using vue3 router
+});
+
+const checkForApiKey = () => {
+  //TODO: use vue router to get the resulting API key
+  const urlParams = new URLSearchParams(window.location.search);
+  const apiKey = urlParams.get('api_key');
+  if (apiKey) {
+    // Store the API key
+    store.keys['openrouter.ai'] = apiKey;
+    $q.notify('API Key retrieved successfully');
+  }
+};
+
+onMounted(() => {
+  checkForApiKey();
+});
+</script>
