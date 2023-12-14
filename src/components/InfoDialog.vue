062da47db1675c2fd258a6c8e03e4c440635ba54
yeus <Thomas.Meschede@web.de>
2023-12-14 11:38:30 -0800

    add info dialog

---
+++ src/components/InfoDialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-btn flat rounded dense icon="info" @click="showInfo = true">
+    <q-dialog v-model="showInfo">
+      <q-card>
+        <q-card-section>
+          <QMarkdown :src="infoText" />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { QMarkdown } from '@quasar/quasar-ui-qmarkdown';
+const showInfo = ref(false);
+
+defineProps({
+  infoText: {
+    type: String,
+    required: true,
+  },
+});
+</script>
