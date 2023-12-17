53548250c9aeb2eadfc4fbca0b7f0522389e666f
yeus <Thomas.Meschede@web.de>
2023-12-16 22:58:27 -0800

    initial taskmanagement layout

--- src/pages/TaskManager.vue
+++ src/pages/TaskManager.vue
@@ -1,10 +1,81 @@
 <template>
   <q-page>
-    <Search></Search>
+    <q-table
+      wrap-cells
+      title="Search Results"
+      :rows="searchResults"
+      :pagination="initialPagination"
+      :columns="[
+        {
+          name: 'id',
+          required: true,
+          label: 'id',
+          field: (row) => row.document.id,
+        },
+        {
+          name: 'score',
+          sortable: true,
+          required: true,
+          label: 'score',
+          field: (row) => 1 / (row.distance + 0.001),
+        },
+        {
+          name: 'document',
+          sortable: true,
+          required: true,
+          label: 'document',
+          field: (row) => row.document.document.pageContent,
+        },
+        {
+          name: 'meta',
+          sortable: true,
+          required: true,
+          label: 'meta',
+          field: (row) => row.document.document.metadata,
+        },
+      ]"
+      row-key="name"
+    >
+      <template v-slot:top>
+        <Search @search="onSearchChange" class="fit" />
+      </template>
+      <template v-slot:body-cell-meta="props">
+        <pre>{{ props }}</pre>
+      </template>
+    </q-table>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import Search from 'src/components/Search.vue';
+import { ref } from 'vue';
+import Search from 'components/Search.vue';
+import { LLMTask } from 'src/modules/types';
 
+const searchResults = ref<LLMTask[]>([]);
+
+function onSearchChange(searchTerm: string | Event, k: number) {
+  if (searchTerm instanceof Event) {
+    // for some reason, in chrome, a second event with the original input-event gets fired...
+    return;
+  } else if (!searchTerm) {
+    searchResults.value = [];
+  } else {
+    // Perform your search here
+    console.log(`Searching for ${searchTerm}`);
+    // searchResults.value = await vectorStore.query(searchTerm, k)
+    console.log(searchResults.value);
+  }
+}
+
+//const numberOfSearchResults = ref(5)
+const initialPagination = {
+  sortBy: 'score',
+  descending: true,
+  //page: 2,
+  rowsPerPage: 5,
+  // rowsNumber: xx if getting data from a server
+};
+function resetDb() {
+  console.log('resetting document store');
+}
 </script>
