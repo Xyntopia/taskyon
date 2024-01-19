b42f7ae45f98e5a6b2b80b1baed4bc5b3b479fff
yeus <Thomas.Meschede@web.de>
2024-01-19 11:14:47 -0800

    make it possible to load settings from yaml & json

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,18 +1,32 @@
 <template>
   <q-page :class="$q.dark.isActive ? 'bg-primary' : 'white'">
-    <div class="q-pa-md">
-      Download Settings:
+    <div class="q-pa-md row items-center q-gutter-sm">
+      <div class="col-auto">Download Settings:</div>
       <q-icon name="save" size="md" />
-      <q-btn
-        label="JSON"
-        @click="downloadSettings('json')"
-        class="q-ma-md"
-      ></q-btn>
-      <q-btn
-        label="YAML"
-        @click="downloadSettings('yaml')"
-        class="q-ma-md"
-      ></q-btn>
+      <q-btn label="JSON" @click="downloadSettings('json')"></q-btn>
+      <q-btn label="YAML" @click="downloadSettings('yaml')"></q-btn>
+      <div>Load Settings:</div>
+      <q-icon name="upload_file" size="md" />
+      <FileDropzone
+        disable-dropzone-border
+        @update:model-value="loadSettingsJson"
+        accept="*"
+      >
+        <q-btn>
+          JSON
+          <q-tooltip>Select Json file for upload!</q-tooltip>
+        </q-btn>
+      </FileDropzone>
+      <FileDropzone
+        disable-dropzone-border
+        @update:model-value="loadSettingsYaml"
+        accept="*"
+      >
+        <q-btn>
+          YAML
+          <q-tooltip>Select YAML file for upload!</q-tooltip>
+        </q-btn>
+      </FileDropzone>
     </div>
     <div class="q-pa-md q-gutter-sm">
       Export app & settings to gdrive:
@@ -59,9 +73,47 @@ import Settings from 'components/Settings.vue';
 import ObjectTreeView from 'components/ObjectTreeView.vue';
 import yaml from 'js-yaml';
 import { onSyncGdrive, onUpdateAppConfiguration } from 'src/modules/gdrive';
+import FileDropzone from 'components/FileDropzone.vue';
+import { deepMergeReactive } from 'src/modules/taskyon/utils';
 
 const tab = ref('settings'); // Default to the first tab
 const state = useTaskyonStore();
+
+// Function to load JSON settings
+// Common function to handle file reading and state updating
+async function loadSettingsFromFile(
+  newFiles: File[],
+  parseFunction: (content: string) => unknown
+) {
+  if (newFiles.length === 0) return; // No file uploaded
+
+  const file = newFiles[0]; // Assuming only one file is uploaded
+
+  try {
+    const fileContent = await file.text();
+    const loadedData = parseFunction(fileContent) as Record<string, unknown>;
+
+    if (loadedData?.chatState) {
+      deepMergeReactive(state.chatState, loadedData.chatState);
+    }
+    if (loadedData?.appConfiguration) {
+      deepMergeReactive(state.appConfiguration, loadedData.appConfiguration);
+    }
+  } catch (error) {
+    console.error('Error processing file', error);
+  }
+  console.log('new configuration loaded:', state)
+}
+
+// Function to load JSON settings
+function loadSettingsJson(newFiles: File[]) {
+  void loadSettingsFromFile(newFiles, JSON.parse);
+}
+
+// Function to load YAML settings
+function loadSettingsYaml(newFiles: File[]) {
+  void loadSettingsFromFile(newFiles, yaml.load);
+}
 
 const downloadSettings = (format: string) => {
   console.log('download settings');
