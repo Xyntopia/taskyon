f6e5aa3b95f97cdf4973f05d1eb4b90bfbeea334
yeus <Thomas.Meschede@web.de>
2024-02-08 22:37:11 -0800

    moe reactoring...

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,4 @@
   <router-view />
 </template>
 
-<script setup lang="ts">
-
-/*const $q = useQuasar()
-
-$q.iconMapFn = (iconName) => {
-const icon = myIcons[iconName]
-if (icon !== void 0) {
-  return { icon: icon }
-}*/
-</script>
+<script setup lang="ts"></script>
