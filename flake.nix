fe7ceb8db1a875d37c518fb81e12adc891fa59ae
yeus <Thomas.Meschede@web.de>
2024-03-13 14:28:36 -0700

    clean nix flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,10 @@
 {
   description = "Nix Development Flake for Quasar";
-  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    # we can not use poetry2nix right now, because we're getting infinite recursion errors.
-    # I suspect, the reason for this is, that one of our dependencies uses a package
-    # which is also included in the nix poetry package...
-    /* poetry2nix = {
-         url = "github:nix-community/poetry2nix";
-         inputs.nixpkgs.follows = "nixpkgs";
-       };
-    */
-    #https://www.reddit.com/r/NixOS/comments/11xgby8/python_and_flake_infinite_recursion/
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -26,8 +16,6 @@
         # if we want a specific rust version:
         # rust-overlay.url = "github:oxalica/rust-overlay";
         pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python310;
-        #pythonPackages = python.pkgs;
 
         # this is all tauri-related stuff
         libraries = with pkgs; [
@@ -87,14 +75,6 @@
           # TODO: what is this for? nativeBuildInputs = [ pkgs.bashInteractive ];
           buildInputs = libraries ++ packages;
           shellHook = ''
-            # Environment variables
-            # fixes libstdc++ issues, libz.so.1 issues
-            # export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:${
-              pkgs.lib.makeLibraryPath buildInputs
-            }";
-
-            #PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
-
             if [ -f ./.env ]; then
               set -a  # automatically export all variables
               source ./.env
@@ -109,11 +89,10 @@
             export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
           '';
           # fixes xcb issues :
-          #QT_PLUGIN_PATH=${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}
+          # QT_PLUGIN_PATH=${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}
 
           # fixes libstdc++ issues and libgl.so issues
           #LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
-          #export LD_LIBRARY_PATH="$(nix eval --raw nixpkgs.zlib)/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH}"
         };
       });
 }
