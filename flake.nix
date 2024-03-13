29bbae5bb8a7b9898354600ac0f553d9c0d2a9f6
yeus <Thomas.Meschede@web.de>
2024-03-13 10:57:00 -0700

    install rust toolchain for tauri

--- flake.nix
+++ flake.nix
@@ -9,38 +9,89 @@
     # we can not use poetry2nix right now, because we're getting infinite recursion errors.
     # I suspect, the reason for this is, that one of our dependencies uses a package
     # which is also included in the nix poetry package...
-    /*poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };*/
+    /* poetry2nix = {
+         url = "github:nix-community/poetry2nix";
+         inputs.nixpkgs.follows = "nixpkgs";
+       };
+    */
     #https://www.reddit.com/r/NixOS/comments/11xgby8/python_and_flake_infinite_recursion/
   };
 
-  outputs =
-    { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
 
-    flake-utils.lib.eachDefaultSystem
-      (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
+        # https://nixos.wiki/wiki/Rust
+        # https://nixos.org/manual/nixpkgs/stable/#rust
+        # if we want a specific rust version:
+        # rust-overlay.url = "github:oxalica/rust-overlay";
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python310;
         #pythonPackages = python.pkgs;
-      in
-      {
+
+        # this is all tauri-related stuff
+        libraries = with pkgs; [
+
+          # tauri deps
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+          libsoup
+
+          # this is needed for appimage by build_appimage.sh ...
+          libgpg-error 
+          xorg.libX11
+          xorg.libSM
+          xorg.libICE
+          xorg.libxcb
+          fribidi
+          fontconfig
+          libthai
+          harfbuzz
+          freetype
+          libglvnd
+          mesa
+          libdrm
+        ];
+        packages = with pkgs; [
+          # rust
+          rustfmt
+          #clippy
+          rustc
+          cargo
+          #cargo-deny
+          #cargo-edit
+          #cargo-watch
+          #llvmPackages.bintools
+          #rustup
+          #rust-analyzer
+          #rust-src
+
+          # for tauri
+          curl
+          wget
+          pkg-config
+
+          # node
+          yarn
+          nodejs_21
+        ];
+      in {
         devShells.default = pkgs.mkShell rec {
           name = "xyntopia_gui";
-          nativeBuildInputs = [ pkgs.bashInteractive ];
-          buildInputs = with pkgs; [
-            # js/ts dependencies
-            yarn
-            nodejs_21
-
-            # TODO: install rust dependencies for buliding the gui!
-          ];
+          # TODO: what is this for? nativeBuildInputs = [ pkgs.bashInteractive ];
+          buildInputs = libraries ++ packages;
           shellHook = ''
             # Environment variables
             # fixes libstdc++ issues, libz.so.1 issues
-            # export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.lib.makeLibraryPath buildInputs}";
+            # export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:${
+              pkgs.lib.makeLibraryPath buildInputs
+            }";
 
             #PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
 
@@ -51,6 +102,11 @@
             fi
 
             export PATH="$(pwd)/node_modules/.bin:$PATH"
+
+            export LD_LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath libraries
+            }:$LD_LIBRARY_PATH
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
           '';
           # fixes xcb issues :
           #QT_PLUGIN_PATH=${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}
