4581b51a3f2142397481b1fa089c9b20391df823
yeus <Thomas.Meschede@web.de>
2024-04-26 09:56:41 -0700

    add under construction message
    Signed-off-by: yeus <Thomas.Meschede@web.de>

--- src/components/ToolManager.vue
+++ src/components/ToolManager.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="column q-pa-md q-gutter-sm">
+    <div class="row q-gutter-sm items-center text-white bg-negative">
+      <q-icon name="science" size="md" />
+      <div>This page is under construction. And not fully functional</div>
+      <q-icon size="md" name="engineering" />
+    </div>
     <q-select
       filled
       dense
