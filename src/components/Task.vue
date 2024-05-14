aebed314a8e7885c0e306e2ffe751ebf77f9d779
yeus <Thomas.Meschede@web.de>
2024-05-14 13:32:48 -0700

    doing some refactoring...

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -43,10 +43,7 @@
             {{ task.content.message }}
           </div>
         </div>
-        <div
-          v-else-if="'uploadedFiles' in task.content"
-          class="col"
-        >
+        <div v-else-if="'uploadedFiles' in task.content" class="col">
           <div>Uploaded files:</div>
           <q-list>
             <q-item v-for="file in fileMappings" :key="file?.uuid">
@@ -97,94 +94,14 @@
         </div>
       </div>
       <!--buttons-->
-      <div
-        class="row justify-start items-stretch message-buttons absolute-bottom-left print-hide rounded-borders"
-      >
-        <q-btn
-          v-if="'message' in task.content"
-          class="col-auto"
-          icon="content_copy"
-          dense
-          flat
-          size="sm"
-          @click="copyToClipboard(task.content.message || '')"
-        >
-          <q-tooltip :delay="0">Copy raw text.</q-tooltip>
-        </q-btn>
-        <q-btn
-          v-if="'message' in task.content"
-          class="col-auto"
-          :icon="
-            state.taskState[task.id]?.markdownEnabled != false
-              ? 'mdi-language-markdown'
-              : 'raw_on'
-          "
-          dense
-          flat
-          size="sm"
-          @click="toggleMarkdown(task.id)"
-        >
-          <q-tooltip :delay="0">Toggle Markdown</q-tooltip>
-        </q-btn>
-        <q-separator
-          v-if="'message' in task.content"
-          vertical
-          class="q-mx-sm"
-        />
-        <q-btn
-          v-if="'message' in task.content"
-          class="col-auto"
-          size="sm"
-          dense
-          flat
-          @click="createNewConversation(task.id)"
-        >
-          <q-icon name="mdi-star-four-points" size="xs"></q-icon>
-          <q-tooltip :delay="0">
-            Start a new thread with this message!
-          </q-tooltip>
-        </q-btn>
-        <q-btn
-          v-if="task.childrenIDs.length > 0"
-          class="col-auto"
-          size="sm"
-          dense
-          flat
-          @click="state.chatState.selectedTaskId = task.id"
-        >
-          <q-icon class="rotate-180" name="alt_route"></q-icon>
-          <q-tooltip :delay="0"
-            >Start alternative conversation branch from here</q-tooltip
-          >
-        </q-btn>
-        <q-btn
-          v-if="'message' in task.content || 'functionCall' in task.content"
-          class="col-auto"
-          icon="edit"
-          dense
-          flat
-          size="sm"
-          @click="editTask(task.id)"
-        >
-          <q-tooltip :delay="0">Edit Task/Message</q-tooltip>
-        </q-btn>
-        <q-separator
-          v-if="state.appConfiguration.expertMode"
-          vertical
-          class="q-mx-sm"
-        />
-        <q-btn
-          v-if="state.appConfiguration.expertMode"
-          class="col-auto"
-          icon="code"
-          dense
-          flat
-          size="sm"
-          @click="toggleMessageDebug(task.id)"
-        >
-          <q-tooltip :delay="0">Show message context</q-tooltip>
-        </q-btn>
-      </div>
+      <TaskButtons
+        :task="task"
+        :state="state"
+        :toggleMarkdown="toggleMarkdown"
+        :createNewConversation="createNewConversation"
+        :editTask="editTask"
+        :toggleMessageDebug="toggleMessageDebug"
+      />
     </div>
     <!--task debugging-->
     <q-slide-transition>
@@ -318,8 +235,8 @@ import type { LLMTask } from 'src/modules/taskyon/types';
 import tyMarkdown from './tyMarkdown.vue';
 import { computed, ref } from 'vue';
 import { FileMappingDocType } from 'src/modules/taskyon/rxdb';
-import InfoDialog from './InfoDialog.vue';
 import { dump } from 'js-yaml';
+import TaskButtons from './TaskButtons.vue';
 
 const props = defineProps<{
   task: LLMTask;
@@ -337,17 +254,6 @@ if ('uploadedFiles' in props.task.content) {
     );
     fileMappings.value = fm;
   })(props.task.content.uploadedFiles);
-}
-
-function copyToClipboard(text: string) {
-  navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      console.log('Copied to clipboard');
-    })
-    .catch((err) => {
-      console.error('Error in copying text: ', err);
-    });
 }
 
 async function taskDraftFromTask(taskId: string) {
---
+++ src/components/TaskButtons.vue
@@ -0,0 +1,110 @@
+<template>
+  <div
+    class="row justify-start items-stretch message-buttons absolute-bottom-left print-hide rounded-borders"
+  >
+    <q-btn
+      v-if="'message' in task.content"
+      class="col-auto"
+      icon="content_copy"
+      dense
+      flat
+      size="sm"
+      @click="copyToClipboard(task.content.message || '')"
+    >
+      <q-tooltip :delay="0">Copy raw text.</q-tooltip>
+    </q-btn>
+    <q-btn
+      v-if="'message' in task.content"
+      class="col-auto"
+      :icon="
+        state.taskState[task.id]?.markdownEnabled != false
+          ? 'mdi-language-markdown'
+          : 'raw_on'
+      "
+      dense
+      flat
+      size="sm"
+      @click="toggleMarkdown(task.id)"
+    >
+      <q-tooltip :delay="0">Toggle Markdown</q-tooltip>
+    </q-btn>
+    <q-separator v-if="'message' in task.content" vertical class="q-mx-sm" />
+    <q-btn
+      v-if="'message' in task.content"
+      class="col-auto"
+      size="sm"
+      dense
+      flat
+      @click="createNewConversation(task.id)"
+    >
+      <q-icon name="mdi-star-four-points" size="xs"></q-icon>
+      <q-tooltip :delay="0"> Start a new thread with this message! </q-tooltip>
+    </q-btn>
+    <q-btn
+      v-if="task.childrenIDs.length > 0"
+      class="col-auto"
+      size="sm"
+      dense
+      flat
+      @click="state.chatState.selectedTaskId = task.id"
+    >
+      <q-icon class="rotate-180" name="alt_route"></q-icon>
+      <q-tooltip :delay="0"
+        >Start alternative conversation branch from here</q-tooltip
+      >
+    </q-btn>
+    <q-btn
+      v-if="'message' in task.content || 'functionCall' in task.content"
+      class="col-auto"
+      icon="edit"
+      dense
+      flat
+      size="sm"
+      @click="editTask(task.id)"
+    >
+      <q-tooltip :delay="0">Edit Task/Message</q-tooltip>
+    </q-btn>
+    <q-separator
+      v-if="state.appConfiguration.expertMode"
+      vertical
+      class="q-mx-sm"
+    />
+    <q-btn
+      v-if="state.appConfiguration.expertMode"
+      class="col-auto"
+      icon="code"
+      dense
+      flat
+      size="sm"
+      @click="toggleMessageDebug(task.id)"
+    >
+      <q-tooltip :delay="0">Show message context</q-tooltip>
+    </q-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { LLMTask } from 'src/modules/taskyon/types';
+import { useTaskyonStore } from 'src/stores/taskyonState';
+
+const state = useTaskyonStore();
+
+defineProps<{
+  task: LLMTask;
+  toggleMarkdown: (id: string) => void;
+  createNewConversation: (taskId: string) => Promise<void>;
+  editTask: (taskId: string) => Promise<void>;
+  toggleMessageDebug: (id: string) => void;
+}>();
+
+function copyToClipboard(text: string) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log('Copied to clipboard');
+    })
+    .catch((err) => {
+      console.error('Error in copying text: ', err);
+    });
+}
+</script>
