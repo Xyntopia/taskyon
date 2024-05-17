d54300b6efc75b30754f7a31d7416ce3efea4bb0
yeus <Thomas.Meschede@web.de>
2024-05-16 23:51:24 -0700

    refactor

---
+++ src/components/GetStarted.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="welcome-message column items-center q-pa-xl">
+    <div class="col-auto">
+      <q-icon
+        class="q-pa-xl"
+        size="10rem"
+        name="svguse:taskyon_mono_opt.svg#taskyon"
+      ></q-icon>
+      <!--<q-img
+	            width="150px"
+	            alt="Quasar logo"
+	            src="~assets/taskyon_mono.svg"
+	          ></q-img>-->
+    </div>
+    <div
+      class="text-h6 col-auto text-center"
+      v-if="state.keys[state.chatState.selectedApi]"
+    >
+      Welcome! Just type a message below to start using Taskyon!
+    </div>
+    <div v-else class="text-h6 col-auto text-center">
+      Welcome to Taskyon! To get started you need to add your API keys to an LLM
+      in the settings!
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTaskyonStore } from 'stores/taskyonState';
+
+const state = useTaskyonStore();
+</script>
--- src/pages/TaskChat.vue
+++ src/pages/TaskChat.vue
@@ -71,30 +71,7 @@
         </div>
       </div>
       <!-- Welcome Message -->
-      <div v-else class="col welcome-message column items-center q-pa-xl">
-        <div class="col-auto">
-          <q-icon
-            class="q-pa-xl"
-            size="10rem"
-            name="svguse:taskyon_mono_opt.svg#taskyon"
-          ></q-icon>
-          <!--<q-img
-            width="150px"
-            alt="Quasar logo"
-            src="~assets/taskyon_mono.svg"
-          ></q-img>-->
-        </div>
-        <div
-          class="text-h6 col-auto text-center"
-          v-if="state.keys[state.chatState.selectedApi]"
-        >
-          Welcome! Just type a message below to start using Taskyon!
-        </div>
-        <div v-else class="text-h6 col-auto text-center">
-          Welcome to Taskyon! To get started you need to add your API keys to an
-          LLM in the settings!
-        </div>
-      </div>
+      <v-else GetStarted />
     </div>
     <!--Create new task area-->
     <q-page-sticky position="bottom" :offset="[0, 0]" expand class="print-hide">
@@ -273,6 +250,7 @@ import OpenRouterPKCE from 'src/components/OpenRouterPKCE.vue';
 import InfoDialog from 'src/components/InfoDialog.vue';
 import axios from 'axios';
 import type { LLMTask } from 'src/modules/taskyon/types';
+import GetStarted from './GetStarted.vue';
 const { getScrollHeight, getScrollTarget, setVerticalScrollPosition } = scroll;
 
 const welcomeText = ref<string>('');
