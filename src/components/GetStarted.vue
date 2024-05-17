4442d818b58c8869441936f7382a2292baef975d
yeus <Thomas.Meschede@web.de>
2024-05-17 00:19:57 -0700

    move api key message into getstarted area...

--- src/components/GetStarted.vue
+++ src/components/GetStarted.vue
@@ -19,14 +19,74 @@
       Welcome! Just type a message below to start using Taskyon!
     </div>
     <div v-else class="text-h6 col-auto text-center">
-      Welcome to Taskyon! To get started you need to add your API keys to an LLM
-      in the settings!
+      <!--API Key hint-->
+      <q-card-section class="q-gutter-md column">
+        <p>
+          No API key found for currently selected API:
+          <span class="text-bold">{{ state.chatState.selectedApi }}</span>
+          . Either choose a different API by pressing one of the following
+          buttons:
+        </p>
+        <div class="self-center q-gutter-md">
+          <q-btn
+            v-for="keyname of Object.keys(state.keys).filter(
+              (k) => state.keys[k]
+            )"
+            :key="keyname"
+            :label="keyname"
+            color="secondary"
+            @click="state.chatState.selectedApi = keyname"
+          />
+        </div>
+        <p>Or Generate a new API key in various ways:</p>
+        <OpenRouterPKCE />
+        <q-item-label header>More Options (not recommended)</q-item-label>
+        <div class="row">
+          <q-btn
+            class="col"
+            label="Go to OpenAI to retrieve an API key from OpenAI (not recommended)"
+            outline
+            noCaps
+            href="https://platform.openai.com/account/api-keys"
+            target="_blank"
+          />
+          <InfoDialog
+            info-text="You can also get an API key from https://openrouter.ai/keys and manually insert into the settings."
+          />
+        </div>
+        <div class="row">
+          <q-btn
+            class="col"
+            label="Access keys via OpenRouter Dashboard (not recommended)"
+            outline
+            noCaps
+            href="https://openrouter.ai/keys"
+            target="_blank"
+          />
+          <InfoDialog
+            info-text="Get an API key from https://www.openrouter.ai and use it for inference tasks"
+          />
+        </div>
+      </q-card-section>
+      <q-card-section>
+        If getting keys manually or for your own server, add them to settings
+        here: The server must be compatible with OpenAI API.
+        <q-btn
+          class="q-mx-sm"
+          dense
+          to="/settings/llmproviders"
+          label="settings"
+          icon="settings"
+        ></q-btn>
+      </q-card-section>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useTaskyonStore } from 'stores/taskyonState';
+import OpenRouterPKCE from 'src/components/OpenRouterPKCE.vue';
+import InfoDialog from 'src/components/InfoDialog.vue';
 
 const state = useTaskyonStore();
 </script>
--- src/pages/TaskChat.vue
+++ src/pages/TaskChat.vue
@@ -14,7 +14,7 @@
         style="background-color: inherit; color: inherit"
         flat
         square
-        v-if="selectedThread.length > 0"
+        v-if="(selectedThread.length > 0) && state.keys[state.chatState.selectedApi]"
       >
         <div v-if="currentTask" class="q-gutter-xs q-px-xs task-container">
           <div v-for="task in selectedThread" :key="task.id" :class="task.role">
@@ -71,7 +71,7 @@
         </div>
       </div>
       <!-- Welcome Message -->
-      <v-else GetStarted />
+      <GetStarted v-else />
     </div>
     <!--Create new task area-->
     <q-page-sticky position="bottom" :offset="[0, 0]" expand class="print-hide">
@@ -93,69 +93,6 @@
           ]"
         >
         </CreateNewTask>
-        <!--API Key hint-->
-        <q-card v-else>
-          <q-card-section class="q-gutter-md column">
-            <p>
-              No API key found for currently selected API:
-              <span class="text-bold">{{ state.chatState.selectedApi }}</span>
-              . Either choose a different API by pressing one of the following
-              buttons:
-            </p>
-            <div class="self-center q-gutter-md">
-              <q-btn
-                outline
-                dense
-                v-for="keyname of Object.keys(state.keys).filter(
-                  (k) => state.keys[k]
-                )"
-                :key="keyname"
-                :label="keyname"
-                @click="state.chatState.selectedApi = keyname"
-              />
-            </div>
-            <p>Or Generate a new API key in various ways:</p>
-            <OpenRouterPKCE />
-            <q-item-label header>More Options (not recommended)</q-item-label>
-            <div class="row">
-              <q-btn
-                class="col"
-                label="Go to OpenAI to retrieve an API key from OpenAI (not recommended)"
-                outline
-                noCaps
-                href="https://platform.openai.com/account/api-keys"
-                target="_blank"
-              />
-              <InfoDialog
-                info-text="You can also get an API key from https://openrouter.ai/keys and manually insert into the settings."
-              />
-            </div>
-            <div class="row">
-              <q-btn
-                class="col"
-                label="Access keys via OpenRouter Dashboard (not recommended)"
-                outline
-                noCaps
-                href="https://openrouter.ai/keys"
-                target="_blank"
-              />
-              <InfoDialog
-                info-text="Get an API key from https://www.openrouter.ai and use it for inference tasks"
-              />
-            </div>
-          </q-card-section>
-          <q-card-section>
-            If getting keys manually or for your own server, add them to
-            settings here: The server must be compatible with OpenAI API.
-            <q-btn
-              class="q-mx-sm"
-              dense
-              to="/settings/llmproviders"
-              label="settings"
-              icon="settings"
-            ></q-btn>
-          </q-card-section>
-        </q-card>
       </div>
     </q-page-sticky>
     <!--Task Chat Control Buttons-->
@@ -246,11 +183,9 @@ import { useQuasar, scroll } from 'quasar';
 import '@quasar/quasar-ui-qmarkdown/dist/index.css';
 import { useTaskyonStore } from 'stores/taskyonState';
 import CreateNewTask from 'components/CreateNewTask.vue';
-import OpenRouterPKCE from 'src/components/OpenRouterPKCE.vue';
-import InfoDialog from 'src/components/InfoDialog.vue';
 import axios from 'axios';
 import type { LLMTask } from 'src/modules/taskyon/types';
-import GetStarted from './GetStarted.vue';
+import GetStarted from 'src/components/GetStarted.vue';
 const { getScrollHeight, getScrollTarget, setVerticalScrollPosition } = scroll;
 
 const welcomeText = ref<string>('');
