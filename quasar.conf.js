78b2604df58b266d4182ac07dbc3985d50051ea7
yeus <Thomas.Meschede@web.de>
2024-06-06 22:09:14 -0700

    re-enable roboto font

--- quasar.conf.js
+++ quasar.conf.js
@@ -103,7 +103,7 @@ module.exports = configure(function (ctx) {
       // 'themify',
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
-      //'roboto-font', // optional, you are not bound to it
+      'roboto-font', // optional, you are not bound to it
       //'material-icons', // optional, you are not bound to it
       //'mdi-v5',
     ],
