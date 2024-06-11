ba3de933811c1fe4c0c037b4f724a18398ae8d49
yeus <Thomas.Meschede@web.de>
2024-06-11 00:37:32 -0700

    found a small mistake in markdownpage routing..  maybe that was the issue?

--- src/router/routes_default.ts
+++ src/router/routes_default.ts
@@ -3,7 +3,7 @@ import { RouteRecordRaw } from 'vue-router';
 export const mdRoutes: RouteRecordRaw[] = [
   {
     path: '/md/:filePath(.*)*',
-    component: import('pages/MarkdownPage.vue'),
+    component: () => import('pages/MarkdownPage.vue'),
     props: (route) => ({
       folder: '', // we use our public folder here for all markdown files :)
       filePath: (route.params.filePath as string[]).join('/') + '.md',
