fa5f3dd79fc8936a6a3153054c6531b2900db864
yeus <Thomas.Meschede@web.de>
2024-06-12 09:30:57 -0700

    different settings layout...
    # Conflicts:
    #	src/pages/Settings.vue

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,69 +1,71 @@
 <template>
   <q-page :class="$q.dark.isActive ? 'bg-primary' : 'white'">
-    <q-tabs :model-value="selectedTab" align="justify">
-      <q-route-tab
-        :to="{ params: { tab: 'llmproviders' } }"
-        exact
-        label="LLM Providers"
-      />
-      <q-route-tab
-        :to="{ params: { tab: 'sync' } }"
-        exact
-        label="Reset & Backup"
-      />
-      <q-route-tab
-        :to="{ params: { tab: 'instructions' } }"
-        exact
-        label="AI/LLM Instructions"
-      />
-      <q-route-tab
-        :to="{ params: { tab: 'agent config' } }"
-        exact
-        label="Agent Configuration"
-      />
-      <q-route-tab
-        v-if="state.appConfiguration.expertMode || selectedTab == 'app config'"
-        :to="{ params: { tab: 'app config' } }"
-        exact
-        label="Expert App Configuration"
-      />
-    </q-tabs>
-    <div class="fit text-center">
-      <ExpertEnable />
+    <div class="row">
+      <div class="col-auto column items-center q-gutter-md">
+        <q-tabs class="col-auto" dense vertical v-model="selectedTab" no-caps>
+          <q-route-tab
+            to="/settings/llmproviders"
+            label="LLM Providers"
+            name="llmproviders"
+          />
+          <q-route-tab to="/settings/sync" label="Reset & Backup" name="sync" />
+          <q-route-tab
+            to="/settings/instructions"
+            label="AI/LLM Instructions"
+            name="instructions"
+          />
+          <q-route-tab
+            to="/settings/agent config"
+            label="Agent Configuration"
+            name="agent config"
+          />
+          <q-route-tab
+            v-if="
+              state.appConfiguration.expertMode || selectedTab == 'app config'
+            "
+            to="/settings/app config"
+            label="Expert App Configuration"
+            name="app config"
+          />
+        </q-tabs>
+        <ExpertEnable />
+      </div>
+      <div class="col" style="min-width: 300px">
+        <q-tab-panels
+          :model-value="selectedTab"
+          animated
+          swipeable
+          infinite
+          :class="[$q.dark.isActive ? 'bg-primary' : 'white']"
+        >
+          <q-tab-panel name="llmproviders" :class="tabPanelClass">
+            <LLMProviders
+              style="max-width: 600px"
+              v-model:expertModeOn="state.appConfiguration.expertMode"
+            />
+          </q-tab-panel>
+          <q-tab-panel name="sync" :class="tabPanelClass">
+            <SyncTaskyon style="max-width: 600px" />
+          </q-tab-panel>
+          <q-tab-panel name="instructions" :class="tabPanelClass">
+            <div>Set custom instructions for the AI Model</div>
+            <ObjectTreeView
+              :model-value="state.llmSettings.taskChatTemplates"
+              class="fit"
+            />
+          </q-tab-panel>
+          <q-tab-panel name="agent config" :class="tabPanelClass">
+            <div>All of the Agent configuration</div>
+            <ObjectTreeView :model-value="state.llmSettings" class="fit" />
+            <!--{{ state.llmSettings }}-->
+          </q-tab-panel>
+          <q-tab-panel name="app config" :class="tabPanelClass">
+            <div>All of the app configurations</div>
+            <ObjectTreeView :model-value="state.appConfiguration" class="fit" />
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
     </div>
-    <q-tab-panels
-      :model-value="selectedTab"
-      animated
-      swipeable
-      infinite
-      :class="[$q.dark.isActive ? 'bg-primary' : 'white']"
-    >
-      <q-tab-panel name="llmproviders" :class="tabPanelClass">
-        <LLMProviders
-          style="max-width: 600px"
-          v-model:expertModeOn="state.appConfiguration.expertMode"
-        />
-      </q-tab-panel>
-      <q-tab-panel name="sync" :class="tabPanelClass">
-        <SyncTaskyon style="max-width: 600px" />
-      </q-tab-panel>
-      <q-tab-panel name="instructions" :class="tabPanelClass">
-        <div>Set custom instructions for the AI Model</div>
-        <ObjectTreeView
-          :model-value="state.llmSettings.taskChatTemplates"
-          class="fit"
-        />
-      </q-tab-panel>
-      <q-tab-panel name="agent config" :class="tabPanelClass">
-        <div>All of the Agent configuration</div>
-        <ObjectTreeView :model-value="state.llmSettings" class="fit" />
-        <!--{{ state.llmSettings }}-->
-      </q-tab-panel>
-      <q-tab-panel name="app config" :class="tabPanelClass">
-        <div>All of the app configurations</div>
-        <ObjectTreeView :model-value="state.appConfiguration" class="fit" />
-      </q-tab-panel>
-    </q-tab-panels>
   </q-page>
 </template>
 
@@ -75,6 +77,7 @@ import ObjectTreeView from 'components/ObjectTreeView.vue';
 import SyncTaskyon from 'components/SyncTaskyon.vue';
 import { useRoute } from 'vue-router';
 import ExpertEnable from './ExpertEnable.vue';
+import { matMenu } from '@quasar/extras/material-icons';
 
 const route = useRoute();
 const state = useTaskyonStore();
