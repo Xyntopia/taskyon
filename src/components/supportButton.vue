429e3b1cb72257479da605824e09fa54e1888077
yeus <Thomas.Meschede@web.de>
2024-06-14 09:52:51 -0700

    add support button

---
+++ src/components/supportButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    href="mailto:support@taskyon.space"
+    :icon="matMail"
+    v-bind="$attrs"
+    target="_blank"
+  >
+  <q-icon :name="matMail"></q-icon>
+  support@taskyon.space
+  </a>
+</template>
+
+<script setup lang="ts">
+import { matMail } from '@quasar/extras/material-icons';
+</script>
