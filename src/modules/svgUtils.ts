3358eeaa83f1a35871f373331ff9696e7b017d95
yeus <Thomas.Meschede@web.de>
2024-06-15 09:40:31 -0700

    image export of svgs is now possible! :)

--- src/modules/svgUtils.ts
+++ src/modules/svgUtils.ts
@@ -1,5 +1,4 @@
 import { initWasm, Resvg } from '@resvg/resvg-wasm';
-//import wasmUrl from '@resvg/resvg-wasm/index_bg.wasm';
 
 const wasmPath = new URL('@resvg/resvg-wasm/index_bg.wasm', import.meta.url);
 
@@ -13,8 +12,64 @@ async function initResvg() {
   }
 }
 
-export async function svgToPng(svgString: string, options = {}) {
+async function loadFont(url: string) {
+  const fontResponse = await fetch(url);
+  if (!fontResponse.ok) {
+    throw new Error('Failed to load font');
+  }
+  const fontData = await fontResponse.arrayBuffer();
+  return new Uint8Array(fontData);
+}
+
+/*
+example options:
+{
+  //fitTo: { mode: 'width', value: 1200 },
+  fitTo: { mode: 'original'},
+  fonts: [new Uint8Array(robotoFont)],
+  defaultFontFamily: { sansSerifFamily: 'Roboto' },
+  scale: 2,
+}
+*/
+
+export async function svgToPng(svgString: string) {
   await initResvg();
+
+  const font = await fetch('./fonts/Roboto-Regular.ttf');
+  if (!font.ok) return;
+
+  const fontData = await font.arrayBuffer();
+  const buffer = new Uint8Array(fontData);
+
+  /*const fontBuffer = await loadFont(
+    '/fonts/KFOmCnqEu92Fr1Mu4mxM.f1e2a767.woff'
+  );*/
+
+  const options: Record<string, unknown> = {
+    fitTo: {
+      mode: 'width', // If you need to change the size
+      value: 1024,
+    },
+    font: {
+      fontBuffers: [buffer], // New in 2.5.0, loading custom fonts
+    },
+  };
+
+  /*const options: Record<string, unknown> = {
+    //fitTo: { mode: 'width', value: 1200 },
+    fitTo: { mode: 'original' },
+    font: { fontBuffers: [fontBuffer] },
+    //defaultFontFamily: { sansSerifFamily: 'Roboto' },
+    //scale: 2,
+  };*/
+
+  // Load custom font if specified in options
+  /*if (options.fontUrl) {
+    const fontBuffer = await loadFont(options.fontUrl);
+    options.font = {
+      fontBuffers: [fontBuffer],
+    };
+  }*/
 
   const resvg = new Resvg(svgString, options);
   const pngData = resvg.render();
