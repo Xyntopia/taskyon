95ef3cb118b001fd5d9a591ac6ba95993092b485
yeus <Thomas.Meschede@web.de>
2024-06-19 23:34:11 -0700

    add a simple toggle button :)

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-btn
+    @click="model = !model"
+    v-bind="$attrs"
+    :class="model ? 'bg-secondary text-primary' : ''"
+    :icon="model ? onIcon : offIcon"
+  >
+    <slot></slot>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import type { QBtnProps } from 'quasar';
+
+const model = defineModel<boolean>({
+  required: false,
+});
+
+defineProps<{
+  onIcon?: QBtnProps['icon'];
+  offIcon?: QBtnProps['icon'];
+}>();
+</script>
