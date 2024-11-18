669e87126411a500d0a8b2f8d4487d2f51b0f6bf
yeus <Thomas.Meschede@web.de>
2024-11-18 10:10:24 -0800

    it looks like our ipfs node can connect now...

--- src/modules/taskyon/ipfs.ts
+++ src/modules/taskyon/ipfs.ts
@@ -1,3 +1,14 @@
+/**
+ * checout these forum posts:  
+ * 
+ * https://discuss.ipfs.tech/t/using-kubo-as-middleman-for-browser-helia-nodes/17138/2
+ * https://discuss.ipfs.tech/t/ipfs-in-browser-use-case/17079
+ * https://discuss.ipfs.tech/t/how-to-retrieve-content-uploaded-via-helia-using-the-ipfs-gateway/16582/5
+ * 
+ * TODO: persist our peer ID in localstorge or something like that. If we use memroy storage, the
+ * peer ID will get regenerated on every reload of the page. 
+ */
+
 import { createHelia } from 'helia';
 import { strings } from '@helia/strings';
 
@@ -10,7 +21,7 @@ const getHeliaInstance = (() => {
     }
     return heliaPromise;
   };
-})();   
+})();
 
 export async function exportToIpfs(txt: string) {
   const node = await getHeliaInstance();
--- src/pages/taskyon/IpfsStatusPage.vue
+++ src/pages/taskyon/IpfsStatusPage.vue
@@ -3,86 +3,208 @@
     <q-page-container>
       <q-page class="q-pa-md">
         <div class="text-h5">IPFS Node Status</div>
+
+        <q-btn outline label="start" @click="node.start()" />
+        <q-btn outline label="stop" @click="node.stop()" />
         <q-list bordered>
           <q-item v-for="(value, key) in status" :key="key" class="q-pa-xs">
             <q-item-section>{{ key }}</q-item-section>
             <q-item-section>{{ value }}</q-item-section>
           </q-item>
         </q-list>
+
+        <div class="text-h5 q-mt-md">Logs</div>
+        <div class="logs q-mt-sm">
+          <q-item v-for="log in logs.slice(10)" :key="log.id" class="q-pa-xs">
+            <q-item-section :style="{ color: log.color }">{{
+              log.content
+            }}</q-item-section>
+          </q-item>
+        </div>
       </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { strings } from '@helia/strings';
-import { createHelia } from 'helia';
 import { ref, onMounted, onUnmounted } from 'vue';
+/** global Helia, Libp2P, ChainsafeLibp2PYamux, Libp2PWebsockets, Libp2PBootstrap, BlockstoreCore, DatastoreCore */
+
+// not imported from skypack,jsdelivr, nor unpkg because of issues.
+// import { noise } from "https://esm.sh/v111/@chainsafe/libp2p-noise@11.0.1/es2022/libp2p-noise.js";
+//import { MemoryDatastore } from 'datastore-core';
+import { MemoryBlockstore } from 'blockstore-core';
+import { LevelDatastore } from 'datastore-level';
+
+import { createHelia as InstantiateHelia } from 'helia';
+import { unixfs } from '@helia/unixfs';
+//import { EventObject } from '@libp2p/interface';
 
 const status = ref<Record<string, unknown>>({});
-const pollingInterval = 2000; // Check status every 5 seconds
+const logs = ref<Array<{ id: string; content: string; color: typeof COLORS }>>(
+  [],
+);
+const pollingInterval = 2000;
 
 let node: Awaited<ReturnType<typeof createHelia>>;
+let interval: NodeJS.Timeout;
 
-const getConnectedPeers = (helia: typeof node) => {
-  const peers = helia.libp2p.getPeers();
-  return {
-    peer_num: peers.length,
-  };
-  /*connectedPeersList.innerHTML = '';
-  for (const peer of peers) {
+const COLORS = {
+  active: '#357edd',
+  success: '#0cb892',
+  error: '#ea5037',
+};
+
+const addLog = (
+  content: string,
+  color: Keys<typeof COLORS> = COLORS.success,
+  id: string = '',
+) => {
+  logs.value.push({ id, content, color });
+};
+
+const getConnectedPeers = async () => {
+  if (!node) return { peer_num: 0 };
+  const peers = node.libp2p.getPeers();
+  /*for (const peer of peers) {
     const peerEl = document.createElement('li');
     peerEl.innerText = peer.toString();
     connectedPeersList.appendChild(peerEl);
   }*/
+  return { peer_num: peers.length };
 };
 
-async function fetchNodeStatus() {
+/*function maToSelector(string) {
+  // #id-ma-/ip4/139.178.91.71/tcp/4001/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN/p2p-circuit/webrtc/p2p/12D3KooWS2Tw7FNsPGBYZ9hBqaa4woNfdfTizgR5y4caqyaogSYh
+  return string.replace(/[/.]/gim, '-');
+}*/
+
+function getMultiaddrs() {
+  if (!node) return { multiaddrList: undefined };
+
+  // multiaddrList.innerHTML = "";
+  // console.log(helia.libp2p.getMultiaddrs().map((ma) => ma.toString()).join(', '))
+  const multiaddrList = node.libp2p.getMultiaddrs();
+  // if (multiaddrs.length === 0) {
+  //   return
+  // }
+  const actualMultiaddrStrings = multiaddrList.map((ma) => ma.toString());
+
+  return { multiaddr: actualMultiaddrStrings };
+}
+
+const fetchNodeStatus = async () => {
   if (!node) return;
 
   try {
     const metrics = node.metrics;
-    status.value = {
-      'node started': node ? true : false,
-      ...getConnectedPeers(node),
-      metrics: (metrics ?? {}) as Record<string, unknown>,
-    };
+    const connectedPeers = await getConnectedPeers();
 
-    //console.log(metrics);
+    let statusText = node.libp2p.status; // ? 'Online' : 'Offline';
+    const dhtMode = await node.libp2p.services.dht.getMode();
+    statusText = `${statusText} - ${
+      dhtMode === 'client' ? 'DHT Client' : 'DHT Server'
+    }`;
+
+    status.value = {
+      ...status.value,
+      'node started': node ? true : false,
+      ...connectedPeers,
+      metrics: metrics ?? {},
+      dhtMode: statusText,
+      ...getMultiaddrs(),
+    };
   } catch (error) {
     console.error('Error fetching node status:', error);
     if (error instanceof Error) {
       status.value = { error: error.message };
     }
   }
-}
+};
 
-let interval: NodeJS.Timeout;
+const addTestFile = async () => {
+  if (!node) return;
+
+  try {
+    addLog('Adding a test file to Helia...', COLORS.active);
+
+    const fs = unixfs(node);
+    const encoder = new TextEncoder();
+    const fileContent = encoder.encode(
+      `Test file content - ${new Date().toISOString()}`,
+    );
+    const fileToAdd = { path: 'testfile.txt', content: fileContent };
+
+    const cid = await fs.addFile(fileToAdd, node.blockstore);
+
+    addLog(`File added: CID ${cid.toString()}`, COLORS.success, cid.toString());
+    addLog(`Preview: https://ipfs.io/ipfs/${cid.toString()}`, COLORS.success);
+  } catch (error) {
+    console.error('Error adding file:', error);
+    addLog('Error adding file', COLORS.error);
+  }
+};
+
+const createHelia = async () => {
+  // https://github.com/ipfs/js-stores
+  //  for different persistence methods
+  // application-specific data lives in the datastore
+  const datastore = new LevelDatastore('helia-example');
+  const blockstore = new MemoryBlockstore();
+
+  const heliaInstance = await InstantiateHelia({
+    datastore,
+    blockstore,
+  });
+  addLog('Created Helia instance', COLORS.success);
+
+  return heliaInstance;
+};
 
 onMounted(async () => {
-  // Set up polling
-  interval = setInterval(fetchNodeStatus, pollingInterval);
+  try {
+    addLog('Creating Helia node...', COLORS.active);
 
-  node = await createHelia();
-  console.log('created helia node...');
-  await node.libp2p.services.dht.setMode('server');
+    node = await createHelia();
+    await node.libp2p.services.dht.setMode('server');
+    console.info('Helia is running');
 
-  const s = strings(node);
+    node.libp2p.addEventListener('peer:discovery', (evt) => {
+      addLog(`Discovered peer ${evt.detail.id.toString()}`);
+    });
 
-  const testcid = await s.add(
-    'hello from tasyon! :)' + new Date().toISOString(),
-  );
+    node.libp2p.addEventListener('peer:connect', (evt) => {
+      addLog(`Connected to ${evt.detail.toString()}`);
+    });
+    node.libp2p.addEventListener('peer:disconnect', (evt) => {
+      addLog(`Disconnected from ${evt.detail.toString()}`);
+    });
 
-  console.log('cid', testcid.toString());
+    console.info('PeerId:', node.libp2p.peerId.toString());
+    status.value['peerid'] = node.libp2p.peerId.toString();
+    addLog('Helia node created successfully', COLORS.success);
 
-  // Initial fetch
-  await fetchNodeStatus();
+    await node.libp2p.services.dht.setMode('server');
+    addLog('DHT mode set to server', COLORS.success);
+
+    interval = setInterval(fetchNodeStatus, pollingInterval);
+
+    // Initial file addition
+    await addTestFile();
+
+    // Fetch status initially
+    await fetchNodeStatus();
+  } catch (error) {
+    console.error('Error initializing Helia node:', error);
+    addLog('Error initializing Helia node', COLORS.error);
+  }
 });
 
 onUnmounted(() => {
   clearInterval(interval);
   if (node) {
-    node.stop(); // Stop Helia node if necessary
+    node.stop();
+    addLog('Helia node stopped', COLORS.active);
   }
 });
 </script>
