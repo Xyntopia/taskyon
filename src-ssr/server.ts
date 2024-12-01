5a238a2a59a33dac44f926e631baef1e1a89e340
yeus <Thomas.Meschede@web.de>
2024-12-01 00:38:06 -0800

    use vite ssr mode :)

--- src-ssr/server.ts
+++ src-ssr/server.ts
@@ -1,6 +1,6 @@
 /**
  * More info about this file:
- * https://v2.quasar.dev/quasar-cli-webpack/developing-ssr/ssr-webserver
+ * https://v2.quasar.dev/quasar-cli-vite/developing-ssr/ssr-webserver
  *
  * Runs in Node context.
  */
@@ -110,7 +110,7 @@ const pngRE = /\.png$/;
  */
 export const renderPreloadTag = defineSsrRenderPreloadTag((file/* , { ssrContext } */) => {
   if (jsRE.test(file) === true) {
-    return `<script src="${file}" defer crossorigin></script>`;
+    return `<link rel="modulepreload" href="${file}" crossorigin>`;
   }
 
   if (cssRE.test(file) === true) {
