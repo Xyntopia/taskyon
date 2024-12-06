00ab2bcaab6a7148a7eda674a429b8c927a89596
yeus <Thomas.Meschede@web.de>
2024-12-05 22:39:55 -0800

    temporary workaround for eslint...

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import pluginVue from 'eslint-plugin-vue'
-import pluginQuasar from '@quasar/app-webpack/eslint'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import prettierSkipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import pluginQuasar from '@quasar/app-webpack/eslint';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import prettierSkipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default [
   {
@@ -33,7 +33,7 @@ export default [
    * pluginVue.configs["flat/recommended"]
    *   -> Above, plus rules to enforce subjective community defaults to ensure consistency.
    */
-  ...pluginVue.configs[ 'flat/essential' ],
+  ...pluginVue.configs['flat/essential'],
 
   // https://github.com/vuejs/eslint-config-typescript
   ...vueTsEslintConfig({
@@ -42,17 +42,24 @@ export default [
     // https://typescript-eslint.io/users/configs#recommended-configurations
     extends: [
       // By default, only the recommended rules are enabled.
-      'recommended'
+      //'disableTypeChecked',
+      'recommended',
+      //'recommendedTypeChecked'
       // You can also manually enable the stylistic rules.
       // "stylistic",
 
       // Other utility configurations, such as 'eslintRecommended', (note that it's in camelCase)
       // are also extendable here. But we don't recommend using them directly.
-    ]
+    ],
   }),
 
   {
     languageOptions: {
+      /*parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },*/
+
       ecmaVersion: 'latest',
       sourceType: 'module',
 
@@ -64,8 +71,8 @@ export default [
         cordova: 'readonly',
         Capacitor: 'readonly',
         chrome: 'readonly', // BEX related
-        browser: 'readonly' // BEX related
-      }
+        browser: 'readonly', // BEX related
+      },
     },
 
     // add your custom rules here
@@ -73,22 +80,22 @@ export default [
       'prefer-promise-reject-errors': 'off',
       '@typescript-eslint/consistent-type-imports': [
         'error',
-        { prefer: 'type-imports' }
+        { prefer: 'type-imports' },
       ],
 
       // allow debugger during development only
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    }
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
   },
 
   {
-    files: [ 'src-pwa/custom-service-worker.ts' ],
+    files: ['src-pwa/custom-service-worker.ts'],
     languageOptions: {
       globals: {
-        ...globals.serviceworker
-      }
-    }
+        ...globals.serviceworker,
+      },
+    },
   },
 
-  prettierSkipFormatting
-]
+  prettierSkipFormatting,
+];
