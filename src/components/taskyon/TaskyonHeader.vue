b4af50fc96efabd1be06f5c486481c7d8f32c20a
yeus <Thomas.Meschede@web.de>
2024-12-10 10:05:16 -0800

    access to diagnostics through about page...

--- src/components/taskyon/TaskyonHeader.vue
+++ src/components/taskyon/TaskyonHeader.vue
@@ -146,6 +146,15 @@
                     like task trees, function execution, and sandboxing. Learn
                     more at taskyon.space.
                   </q-card-section>
+                  <q-card-actions>
+                    <q-btn flat color="secondary" to="/diagnostics">
+                      <div class="q-pr-md">
+                        Open Diagnostics
+                      </div>
+                      <q-icon :name="mdiWrench"></q-icon>
+                      <q-icon :name="mdiHospital" size="md"></q-icon>
+                  </q-btn>
+                </q-card-actions>
                   <q-card-section class="text-info" style="font-size: 0.75em">
                     <div
                       v-for="[name, value] of Object.entries(environmentInfo())"
@@ -214,7 +223,9 @@ import {
   mdiForum,
   mdiForumPlus,
   mdiGithub,
+  mdiHospital,
   mdiInformationVariant,
+  mdiWrench,
 } from '@quasar/extras/mdi-v6';
 import { useAppStateStore } from 'src/stores/appState';
 import { ref } from 'vue';
