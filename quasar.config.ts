37e0add6449cd8fe98903d3e3b31fe752d196d5f
yeus <Thomas.Meschede@web.de>
2024-12-11 22:10:08 -0800

    clean up a little

--- quasar.config.ts
+++ quasar.config.ts
@@ -62,10 +62,10 @@ export default defineConfig((ctx) => {
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
       // publicPath: '/',
-      // analyze: true,
+      analyze: true,
       // env: {},
       // rawDefine: {}
-      // ignorePublicFolder: true,
+      ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
       // distDir
--- src-ssr/middlewares/gdrive_proxy.ts
+++
@@ -1,7 +0,0 @@
-import { defineSsrMiddleware } from '#q-app/wrappers'
-
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli-vite/developing-ssr/ssr-middleware
-export default defineSsrMiddleware(async ({ app /*, resolveUrlPath, publicPath, render */ }) => {
-  // something to do with the server "app"
-})
