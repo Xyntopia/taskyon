b1a6bf5bd58c8f14e1699ec3dcc3be0e195289e6
yeus <Thomas.Meschede@web.de>
2025-02-15 09:37:41 -0800

    update taskplanner temporarily to handle lint errors..

--- src/modules/tools/TaskPlannerTool.ts
+++ src/modules/tools/TaskPlannerTool.ts
@@ -1,58 +1,26 @@
-import type { TyTaskManager } from '../taskyon/taskManager'
-import { type TaskWorkerController } from '../taskyon/taskWorker'
-import { TaskProcessingError, type llmSettings } from '../taskyon/types'
-import {
-  makeTaskResult,
-  type InternalTool,
-  type internalToolFunctionSchema,
-  type toolContext,
-} from '../taskyon/tools'
+import { createTool, type toolContext } from '../taskyon/tools'
 
-// TODO: for configuration & allowedTools it would be good if we could add
-// this from a "default" Configuration? And then have them as function parameters?
-// t.configuration = finishedTask.configuration
-export function createTaskPlannerTool(
-  llmSettings: llmSettings,
-  taskManager: TyTaskManager,
-  taskWorkerController: TaskWorkerController,
-  apiKeys: { [key: string]: string },
-): InternalTool {
-  const taskPlanner: internalToolFunctionSchema = async (
-    { allowedTools }: { allowedTools: string[] },
-    context: toolContext,
-  ) => {
-    if (!context.currentTask) {
-      throw new Error(`No current task found!`)
-    }
-    if (!llmSettings.selectedApi) {
-      throw new TaskProcessingError('No API selected!')
-    }
+// TODO: update short & long description so that an LLM AI can use this tool
+// TODO: make more than 1 level of subtasks possible. so taht we immediatly create sub & subsub tasks..
+export const taskPlannerTool = createTool({
+  function: ({ maxSubTaskNum }, context: toolContext) => {
+    console.log(`Split task into ${maxSubTaskNum} subtasks`, context)
     return ''
-  }
-
-  // TODO: update short & long description so that an LLM AI can use this tool
-  const taskPlannerTool: InternalTool = {
-    function: taskPlanner,
-    description: 'Generates a chat-based response using the OpenAI API.',
-    longDescription: `This tool interfaces with an OpenAI-compatible API to generate completions for
-  conversation prompts. Useful for generating natural language responses in a chat setting.`,
-    name: 'chatCompletion',
-    renderOptions: { hideLlm: true },
-    parameters: {
-      type: 'object',
-      properties: {
-        allowedTools: {
-          type: 'array',
-          items: { type: 'string' },
-          description: 'A list of tools that the LLM can use.',
-          default: [],
-        },
+  },
+  description:
+    'This tool can be used to split up a task into sub-tasks which then get worked on each individually',
+  longDescription: `TODO...`,
+  name: 'taskPlanner',
+  renderOptions: { hideLlm: true },
+  parameters: {
+    type: 'object',
+    properties: {
+      maxSubTaskNum: {
+        type: 'integer',
+        description: 'Maximum number of subtasks to create.',
+        default: 5,
       },
-      required: [],
     },
-  }
-
-  return taskPlannerTool
-}
-
-export type chatCompletionTool = ReturnType<typeof createTaskPlannerTool>
+    required: [],
+  } as const,
+})
