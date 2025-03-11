11b6f0b14c3484eddf6681f60deceb00cd9ad72b
yeus <Thomas.Meschede@web.de>
2025-03-10 23:48:07 -0700

    more refactoring...

--- src/modules/taskyon/iframeApi.ts
+++ src/modules/taskyon/iframeApi.ts
@@ -10,6 +10,8 @@ import { match } from 'ts-pattern'
     return typeof obj === 'string' ? obj : JSON.stringify(obj);
   }*/
 
+type TyMessage = MessageEvent<TaskyonMessage>
+
 export function setupIframeApi(
   taskManager: TyTaskManager,
   appConfiguration: Record<string, unknown>,
@@ -20,7 +22,7 @@ export function setupIframeApi(
   // Listen for messages from the parent page
   window.addEventListener(
     'message',
-    function (event: MessageEvent<TaskyonMessage>) {
+    function (event: TyMessage) {
       // Check if the iframe is not the top-level window
       if (window !== window.top) {
         // Check if the message is from the parent window
@@ -39,51 +41,15 @@ export function setupIframeApi(
             const res = TaskyonMessage.safeParse(event.data)
             if (res.success) {
               match(res.data)
-                .with({ type: 'task' }, (msg) => {
-                  console.log(`task was sent by ${event.origin}`, msg.task)
-                  void taskManager
-                    .addPartialTask2Tree(
-                      { ...msg.task, label: [event.origin] },
-                      undefined,
-                      undefined,
-                      false,
-                    )
-                    .catch((err) => console.warn(err))
-                })
-                .with({ type: 'functionDescription' }, (msg) => {
-                  const { id, duplicateTaskName, ...rest } = msg
-                  const newFunc: ToolBase = rest
-                  console.log(`functionDescription was sent by ${event.origin}`, newFunc)
-                  const newTask: partialTaskDraft = {
-                    role: 'system',
-                    name: id,
-                    content: {
-                      type: 'tooldefinition',
-                      data: newFunc,
-                    },
-                    label: [event.origin],
-                  }
-                  void taskManager
-                    .addPartialTask2Tree(newTask, undefined, undefined, duplicateTaskName)
-                    .catch((err) => console.warn(err))
-                })
-                .with({ type: 'configurationMessage' }, (msg) => {
-                  const newConfig = msg.conf
-                  console.log('setting our configuration')
-                  if (newConfig.llmSettings) {
-                    // TODO: add an "update settings" function which can also handle
-                    //       side effects such as setting the taskDrafts etc...
-                    deepMergeReactive(llmSettings, newConfig.llmSettings, 'overwrite')
-                    deepMergeReactive(appConfiguration, newConfig.appConfiguration, 'overwrite')
-                  }
-                  // and also set a possible signature as the api key!
-                  if (llmSettings.selectedApi && newConfig.signatureOrKey) {
-                    // we only set the API key, if it was provided by the
-                    // parent app.
-                    const newKey = newConfig.signatureOrKey
-                    keys[llmSettings.selectedApi] = newKey
-                  }
-                })
+                .with({ type: 'task' }, addNewTask(event, taskManager))
+                .with(
+                  { type: 'functionDescription' },
+                  addNewFunctionDescription(event, taskManager),
+                )
+                .with(
+                  { type: 'configurationMessage' },
+                  setConfiguration(llmSettings, appConfiguration, keys),
+                )
               // we don't need "otherwise" here, because the other messages are currently handled by our
               // remotefunctionhandler
               // TODO:  BUT we want to chane this, and integrate the remote function handler with this API here as well...
@@ -107,4 +73,57 @@ export function setupIframeApi(
 
   const readyMessage: TaskyonMessage = { type: 'taskyonReady' }
   window.parent.postMessage(readyMessage, '*')
+}
+
+function addNewTask(event: TyMessage, taskManager: TyTaskManager) {
+  return (msg: Extract<TaskyonMessage, { type: 'task' }>) => {
+    console.log(`task was sent by ${event.origin}`, msg.task)
+    void taskManager
+      .addPartialTask2Tree({ ...msg.task, label: [event.origin] }, undefined, undefined, false)
+      .catch((err: unknown) => console.warn(err))
+  }
+}
+
+function addNewFunctionDescription(event: TyMessage, taskManager: TyTaskManager) {
+  return (msg: Extract<TaskyonMessage, { type: 'functionDescription' }>) => {
+    const { id, duplicateTaskName, ...rest } = msg
+    const newFunc: ToolBase = rest
+    console.log(`functionDescription was sent by ${event.origin}`, newFunc)
+    const newTask: partialTaskDraft = {
+      role: 'system',
+      name: id,
+      content: {
+        type: 'tooldefinition',
+        data: newFunc,
+      },
+      label: [event.origin],
+    }
+    void taskManager
+      .addPartialTask2Tree(newTask, undefined, undefined, duplicateTaskName)
+      .catch((err: unknown) => console.warn(err))
+  }
+}
+
+function setConfiguration(
+  llmSettings: llmSettings,
+  appConfiguration: Record<string, unknown>,
+  keys: Record<string, string>,
+) {
+  return (msg: Extract<TaskyonMessage, { type: 'configurationMessage' }>) => {
+    const newConfig = msg.conf
+    console.log('setting our configuration')
+    if (newConfig.llmSettings) {
+      // TODO: add an "update settings" function which can also handle
+      //       side effects such as setting the taskDrafts etc...
+      deepMergeReactive(llmSettings, newConfig.llmSettings, 'overwrite')
+      deepMergeReactive(appConfiguration, newConfig.appConfiguration, 'overwrite')
+    }
+    // and also set a possible signature as the api key!
+    if (llmSettings.selectedApi && newConfig.signatureOrKey) {
+      // we only set the API key, if it was provided by the
+      // parent app.
+      const newKey = newConfig.signatureOrKey
+      keys[llmSettings.selectedApi] = newKey
+    }
+  }
 }
