0474df7eb4d5ff22a0dbc1c329b984bbaadd4535
yeus <Thomas.Meschede@web.de>
2025-03-14 14:43:35 -0700

    add a password request dialog...

---
+++ src/components/PasswordRequestDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-dialog v-model="showInfo">
+    <q-card>
+      <q-card-section>
+        <QMarkdown v-if="infoText" :src="infoText" />
+        <slot></slot>
+      </q-card-section>
+      <q-card-section>
+        <SecretInput
+          placeholder="Add Secret key here!"
+          filled
+          v-model="newSecret"
+          label="New Secret"
+        >
+        </SecretInput>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { QMarkdown } from '@quasar/quasar-ui-qmarkdown'
+import SecretInput from './SecretInput.vue'
+
+const newSecret = ref<string>('')
+const showInfo = ref(false)
+
+defineProps({
+  infoText: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+})
+</script>
