ba3df3c9ce14aa6d9e8b0ef8d91f40a2ff297762
yeus <Thomas.Meschede@web.de>
2025-03-16 23:21:51 -0700

    some smaller refactoring...

--- src/modules/crypto_js.ts
+++ src/modules/crypto_js.ts
@@ -151,13 +151,10 @@ export function urlSafeBase64Uuid() {
   return base64Uuid
 }
 
-export async function sha256UrlSafeHash(obj: unknown) {
+export function sha256UrlSafeHash(obj: unknown) {
   const json = JSON.stringify(obj)
-  const encoder = new TextEncoder()
-  const data = encoder.encode(json)
-
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
-  return urlSafe64BitString(Buffer.from(hashBuffer))
+  const h1a = sha256(json)
+  return Promise.resolve(urlSafe64BitString(Buffer.from(h1a)))
 } // Generate a new seed phrase (mnemonic)
 
 export function generateSalt(): string {
