44e57c6af62ea2fa411e90c5b3781631f3de0ad9
yeus <Thomas.Meschede@web.de>
2025-04-13 23:42:23 -0700

    some more refactoring...

--- src/modules/pglite.api.ts
+++ src/modules/pglite.api.ts
@@ -1,6 +1,8 @@
 import { PGliteWorker } from '@electric-sql/pglite/worker'
 import type { LiveNamespace } from '@electric-sql/pglite/live'
 import { live } from '@electric-sql/pglite/live'
+import { sha256UrlSafeHash } from './crypto_webcrypto'
+import { useNlpWorker } from './taskyon/webWorkerApi'
 
 export type TyPGDB = PGliteWorker & { live: LiveNamespace }
 
@@ -69,3 +71,67 @@ export async function createVecPgLiteTable(db: TyPGDB, options: PgLiteOptions) {
 
   return { dataColumn, tableName, idColumn }
 } // TODO: add protections against SQL injection...
+
+const getVectorStoreTable = async (name: string, modelName: string) => {
+  const db = await getDatabase('taskyon')
+  const { vectorizeText } = useNlpWorker()
+  const numDimensions = 384
+
+  const tableInstance = await createVecPgLiteTable(db, {
+    tableName: name,
+    idColumn: 'id',
+    dataColumn: 'data',
+    additionalColumns: ['label TEXT'],
+    pgvector: true,
+    vectorDims: numDimensions,
+  })
+
+  console.log('created', tableInstance)
+
+  return { db, vectorizeText, modelName }
+}
+
+export const createVectorStore = async () => {
+  const modelName = 'xyntopia/all-MiniLM-L6-v2'
+
+  const { db, vectorizeText } = await getVectorStoreTable('vectorStoreTool', modelName)
+
+  const search = async (searchText: string, k: number, label?: string) => {
+    console.log(`Searching for ${searchText}`)
+    const searchVector = await vectorizeText(searchText, modelName)
+    const formattedVector = `[${searchVector.join(',')}]` // Format the array as a string for pgvector
+    if (searchVector) {
+      const results = await db.query(
+        `
+      SELECT
+      label,
+      data,
+      vec <-> $1 AS distance
+      FROM vectorStoreTool
+      WHERE label = $3
+      ORDER BY distance
+      LIMIT $2;
+      `,
+        [formattedVector, k, label],
+      )
+      return results.rows
+    }
+  }
+
+  const insert = async (saveText: string, label?: string) => {
+    const vector = await vectorizeText(saveText, modelName)
+    const formattedVector = `[${vector.join(',')}]` // Format the array as a string for pgvector
+    const id = await sha256UrlSafeHash(saveText)
+    await db.query(
+      `
+      INSERT INTO vectorStoreTool (id, label, data, vec)
+      VALUES ($1, $2, $3, $4);
+    `,
+      // TODO: can this be made more efficient without converting
+      //       the vector to a string?
+      [id, label, JSON.stringify(saveText), formattedVector],
+    )
+  }
+
+  return { search, insert }
+}
--- src/modules/tools/localVectorStore.ts
+++ src/modules/tools/localVectorStore.ts
@@ -1,75 +1,8 @@
+import { createVectorStore } from '../pglite.api'
 import { createTool } from '../taskyon/tools'
-import { createVecPgLiteTable, getDatabase } from '../pglite.api'
-import { useNlpWorker } from '../taskyon/webWorkerApi'
-import { sha256UrlSafeHash } from '../crypto_webcrypto'
 
 // TODO: make it possible to add "unique" identifiers as labels, so that
 //       we can save text which was add from a task and use the task id as the UID
-
-const getVectorStoreTable = async (name: string, modelName: string) => {
-  const db = await getDatabase('taskyon')
-  const { vectorizeText } = useNlpWorker()
-  const numDimensions = 384
-
-  const tableInstance = await createVecPgLiteTable(db, {
-    tableName: name,
-    idColumn: 'id',
-    dataColumn: 'data',
-    additionalColumns: ['label TEXT'],
-    pgvector: true,
-    vectorDims: numDimensions,
-  })
-
-  console.log('created', tableInstance)
-
-  return { db, vectorizeText, modelName }
-}
-
-const createVectorStore = async () => {
-  const modelName = 'xyntopia/all-MiniLM-L6-v2'
-
-  const { db, vectorizeText } = await getVectorStoreTable('vectorStoreTool', modelName)
-
-  const search = async (searchText: string, k: number, label?: string) => {
-    console.log(`Searching for ${searchText}`)
-    const searchVector = await vectorizeText(searchText, modelName)
-    const formattedVector = `[${searchVector.join(',')}]` // Format the array as a string for pgvector
-    if (searchVector) {
-      const results = await db.query(
-        `
-      SELECT
-      label,
-      data,
-      vec <-> $1 AS distance
-      FROM vectorStoreTool
-      WHERE label = $3
-      ORDER BY distance
-      LIMIT $2;
-      `,
-        [formattedVector, k, label],
-      )
-      return results.rows
-    }
-  }
-
-  const insert = async (saveText: string, label?: string) => {
-    const vector = await vectorizeText(saveText, modelName)
-    const formattedVector = `[${vector.join(',')}]` // Format the array as a string for pgvector
-    const id = await sha256UrlSafeHash(saveText)
-    await db.query(
-      `
-      INSERT INTO vectorStoreTool (id, label, data, vec)
-      VALUES ($1, $2, $3, $4);
-    `,
-      // TODO: can this be made more efficient without converting
-      //       the vector to a string?
-      [id, label, JSON.stringify(saveText), formattedVector],
-    )
-  }
-
-  return { search, insert }
-}
-
 export const localVectorStore = createTool({
   name: 'localVectorStore',
   description: `This tool can store Information and perform semantic search in a vector database, ideal
