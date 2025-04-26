ebe70efc3adc7cc4b67867e07b9ee1a7d6715cd6
yeus <Thomas.Meschede@web.de>
2025-04-25 23:56:54 -0700

    remoe logging for now and selectivly display logs in our chat window..

--- src/stores/taskyonState.ts
+++ src/stores/taskyonState.ts
@@ -213,11 +213,13 @@ export const useTaskyonStore = defineStore('taskyonControl', () => {
   const workerStreamLogs = ref<(TyTaskStreamData & { timestamp: Date })[]>([])
   const maxLogRows = 50
   void workerStream.subscribe((data) => {
-    console.log('worker:', data)
-    workerStreamLogs.value.push({ ...data, timestamp: new Date() })
-    // Ensure the log doesn't exceed the maximum number of rows
-    if (workerStreamLogs.value.length > maxLogRows) {
-      workerStreamLogs.value.shift() // Remove the oldest entry
+    // console.log('worker:', data)
+    if (['all finished', 'processing', 'processed', 'error'].includes(data.stage)) {
+      workerStreamLogs.value.push({ ...data, timestamp: new Date() })
+      // Ensure the log doesn't exceed the maximum number of rows
+      if (workerStreamLogs.value.length > maxLogRows) {
+        workerStreamLogs.value.shift() // Remove the oldest entry
+      }
     }
   })
 
