8efb47a3916e98c6a7083e19ffcf2c9d46cd1288
yeus <Thomas.Meschede@web.de>
2025-05-02 12:17:38 -0700

    add a new tool page...

---
+++ src/pages/taskyon/ToolPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh LpR lfr">
+    <TaskyonHeader :min-mode="false" btn-size="md" :drawer-open="undefined" />
+
+    <q-page-container>
+      <q-page padding> Tool Page {{ name }} </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import TaskyonHeader from '../../components/taskyon/TaskyonHeader.vue'
+
+defineProps<{
+  name: string
+}>()
+</script>
