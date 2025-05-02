0d3c89a619ebc19b9dd47794aa6ecc68a4e71b87
yeus <Thomas.Meschede@web.de>
2025-05-02 16:00:44 -0700

    update tool page :)

--- src/pages/taskyon/ToolPage.vue
+++ src/pages/taskyon/ToolPage.vue
@@ -1,7 +1,10 @@
 <template>
-  <q-layout view="lHh LpR lfr">
-    <TaskyonHeader :min-mode="false" btn-size="md" :drawer-open="undefined" />
-
+  <q-layout view="hHh lpR lFr">
+    <TaskyonHeader :min-mode="false" btn-size="md" v-model:drawer-open="drawerOpen" />
+    <q-drawer v-model="drawerOpen" show-if-above persistent behaviour="desktop" :width="250">
+      tst
+      <ObjectTreeView :model-value="functionArgs" />
+    </q-drawer>
     <q-page-container>
       <q-page padding> Tool Page {{ name }} </q-page>
     </q-page-container>
@@ -9,9 +12,15 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import TaskyonHeader from '../../components/taskyon/TaskyonHeader.vue'
+import ObjectTreeView from 'src/components/ObjectTreeView.vue'
 
 defineProps<{
   name: string
 }>()
+
+const functionArgs = ref<Record<string, unknown>>({})
+
+const drawerOpen = ref(false)
 </script>
