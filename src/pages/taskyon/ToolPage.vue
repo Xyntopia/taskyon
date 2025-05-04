e83829070c8a2d6ca34198d40b19f3cfc91f101c
yeus <Thomas.Meschede@web.de>
2025-05-04 10:32:31 -0700

    add CreateNewTask area to our toolpage...

--- src/pages/taskyon/ToolPage.vue
+++ src/pages/taskyon/ToolPage.vue
@@ -2,7 +2,11 @@
   <q-layout view="hHh lpR lFr">
     <TaskyonHeader :min-mode="false" btn-size="md" v-model:drawer-open="drawerOpen" />
     <q-drawer v-model="drawerOpen" show-if-above persistent behaviour="desktop" :width="250">
-      tst
+      <CreateNewTask
+        :force-task-props="state.llmSettings.taskTemplate"
+        class="q-pa-xs"
+        :hide-task-info="state.minimalGui"
+      />
       <ObjectTreeView :model-value="functionArgs" />
     </q-drawer>
     <q-page-container>
@@ -15,10 +19,14 @@
 import { ref } from 'vue'
 import TaskyonHeader from '../../components/taskyon/TaskyonHeader.vue'
 import ObjectTreeView from 'src/components/ObjectTreeView.vue'
+import CreateNewTask from 'src/components/taskyon/CreateNewTask.vue'
+import { useAppStateStore } from 'src/stores/appState'
 
 defineProps<{
   name: string
 }>()
+
+const state = useAppStateStore()
 
 const functionArgs = ref<Record<string, unknown>>({})
 
