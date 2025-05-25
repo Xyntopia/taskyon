b451559d5e10185d4b638dfa9dd073c325580069
SushaanthSrinivasan <iamsushaanth@gmail.com>
2025-05-24 23:11:02 -0700

    feat: rag tools with just vector store operations

--- src/modules/taskyon/init.ts
+++ src/modules/taskyon/init.ts
@@ -23,6 +23,7 @@ import { localVectorStore } from '../tools/localVectorStore'
 import { proceduralTools } from '../tools/proceduralGraphics'
 import { wfcGenerator } from '../tools/wavefunctioncollapse'
 import { createOAuthTool } from '../tools/authTools'
+import { ragSearchTool, ragAddTool } from '../tools/ragTool'
 import type { EncryptedDataRow } from '../crudWrapper'
 import {
   createCombinedCrudWrapper,
@@ -58,6 +59,8 @@ export async function initTaskyon(
     localVectorStore,
     executeJavaScript,
     toolCreationWizard,
+    ragSearchTool,
+    ragAddTool,
     ...EnvironmentTools,
   ]
 
