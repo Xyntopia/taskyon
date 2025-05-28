87f37ad1c30a09f99c4c3aea62420b747cdeed62
yeus <Thomas.Meschede@web.de>
2025-05-27 23:29:36 -0700

    make sure, we can aso see the task creation date in our task manager :)

--- src/pages/taskyon/TaskManager.vue
+++ src/pages/taskyon/TaskManager.vue
@@ -76,7 +76,14 @@
               </div>
             </div>
             <div class="col q-pa-xs">
-              <div class="text-caption text-right">id: {{ rows.row.taskId }}</div>
+              <div class="text-caption text-right">
+                id: {{ rows.row.taskId }} created:
+                {{
+                  taskDataMap[rows.row.taskId]?.created_at != null
+                    ? new Date(taskDataMap[rows.row.taskId]?.created_at!).toLocaleString()
+                    : ''
+                }}
+              </div>
               <Task v-if="taskDataMap[rows.row.taskId]" :task="taskDataMap[rows.row.taskId]!" />
               <div v-else>We could not find the task, is it possible that it was deleted?</div>
             </div>
