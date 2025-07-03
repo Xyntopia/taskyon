5d199200e20fb333bc14d4bc1b0d844f5c2afedd
yeus <Thomas.Meschede@web.de>
2025-07-02 20:25:15 -0700

    update flake...   we want the new tauri version...

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, nixpkgs_unstable, flake-utils }:
