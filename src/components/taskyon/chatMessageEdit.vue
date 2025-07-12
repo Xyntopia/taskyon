02cf10418fcc4aa52a667ecfaf16b1018f4cdc95
yeus <Thomas.Meschede@web.de>
2025-07-11 22:29:25 -0700

    better colors for input field :)

--- src/components/taskyon/chatMessageEdit.vue
+++ src/components/taskyon/chatMessageEdit.vue
@@ -1,17 +1,25 @@
 <template>
+  <!--edit chat messages-->
   <q-input
     v-model.trim="content"
     autogrow
-    filled
+    borderless
     color="secondary"
-    label="Type your message or instruction..."
+    placeholder="Type your message or instruction..."
     clearable
     input-style="max-height: 300px"
     v-bind="$attrs"
     @keyup="checkKeyboardEvents"
   >
     <template #append>
-      <q-btn flat dense round :icon="matSend" @click="$emit('execute-task')">
+      <q-btn
+        flat
+        dense
+        round
+        :icon="matSend"
+        :color="$q.dark.isActive ? 'white' : 'dark'"
+        @click="$emit('execute-task')"
+      >
         <q-tooltip>
           Press to send or alternatively send with &lt;shift&gt; + &lt;enter&gt;
         </q-tooltip>
