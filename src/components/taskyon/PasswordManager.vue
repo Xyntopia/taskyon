3f10adaaf11959b90c4e2068652d3a3639b83914
yeus <Thomas.Meschede@web.de>
2025-07-15 16:18:40 -0700

    update password manager with better names and references to actual tools

--- src/components/taskyon/PasswordManager.vue
+++ src/components/taskyon/PasswordManager.vue
@@ -3,7 +3,7 @@
     <q-item v-for="(secretRow, secretId) in secretList" :key="secretId">
       <q-expansion-item
         dense
-        :label="secretId.split(':')[0]"
+        :label="toolMap[secretId] ?? secretId.split(':')[0]"
         default-opened
         header-class="text-h6"
         class="fit"
@@ -28,6 +28,10 @@
           ></q-btn>
         </q-item>
       </q-expansion-item>
+      <q-item-section side top>
+        <q-btn flat label="Tool Manager" :to="'/tool/' + toolMap[secretId]" />
+        <q-btn flat color="negative" label="delete all" @click="deleteAllSecrets(secretId)"></q-btn>
+      </q-item-section>
     </q-item>
   </q-list>
 </template>
@@ -37,6 +41,8 @@ import { useTaskyonStore } from 'src/stores/taskyonState'
 import SecretInput from '../SecretInput.vue'
 import { matDeleteForever, matSave } from '@quasar/extras/material-icons'
 import { onMounted, ref } from 'vue'
+import { asyncComputed } from 'src/modules/vueUtils'
+import { generateSecretId } from 'src/modules/taskyon/taskWorker'
 
 const tystate = useTaskyonStore()
 
@@ -60,6 +66,11 @@ const deleteSecrets = async (secretId: string, secretName: string) => {
   await loadSecrets()
 }
 
+const deleteAllSecrets = async (secretId: string) => {
+  await tystate.secretStore.deleteAllFromId(secretId)
+  await loadSecrets()
+}
+
 // only called on Enter or Save‑button
 const saveSecret = async (secretId: string, secretName: string) => {
   const newVal = secretList.value[secretId]![secretName]
@@ -67,4 +78,21 @@ const saveSecret = async (secretId: string, secretName: string) => {
   // optional: refocus or toast here
   await loadSecrets()
 }
+
+const toolMap = asyncComputed(async () => {
+  const allTools = await tystate.getAllTools()
+  if (allTools) {
+    const tm = await tystate.getTaskManager()
+    const tools: Record<string, string> = {}
+    for (const c of Object.values(allTools)) {
+      const { tool, def } = await tm.getToolDefinition(c.name)
+      if (tool) {
+        const id = await generateSecretId(def?.id, tool)
+        tools[id] = def?.id ?? c.name
+      }
+    }
+    return tools
+  }
+  return {}
+}, {})
 </script>
