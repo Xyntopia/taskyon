407adb42b892e0d64132e8b7931fc4e6ac5fd912
yeus <Thomas.Meschede@web.de>
2025-07-17 00:26:46 -0700

    libgbm has been moved to a new package...

--- flake.nix
+++ flake.nix
@@ -90,7 +90,8 @@
           xorg.libxcb
           libxkbcommon
           #xorg
-          mesa # for libgbm
+          mesa 
+          libgbm # for libgbm
           expat
         ];
         packages = with pkgs; [
@@ -135,26 +136,40 @@
           # not sure..  but if I use our git-filter repo form main nix store, it doesn't work.. maybe becuase of cinflicting python versions?
           # git-filter-repo 
           kdiff3
+
+          # we use this to search for missing libraries with the same version as this
+          # project here.
+          # run: 
+          # > nix-index
+          # > nix-locate libgbm.so.1
+          nix-index
         ];
       in {
         devShells.default = pkgs.mkShell rec {
-          pure  = true;                   # ← turn on “pure” mode
+          # pure  = true;                   # ← turn on “pure” mode
           name = "xyntopia_gui";
           # TODO: what is this for? nativeBuildInputs = [ pkgs.bashInteractive ];
-          buildInputs = packages;
+          buildInputs = libraries ++ packages;
           # the following comes from here: https://tauri.app/start/prerequisites/
           # but by declaring LD_LIBRARY_PATH we might have done it correctly already ;)
           # and thats why we're commenting it out...
           #PKG_CONFIG_PATH = "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig";
-          propagatedBuildInputs = libraries;  # your GTK/WebKit/etc libs
+          # propagatedBuildInputs = libraries;  # your GTK/WebKit/etc libs
           
           shellHook = ''
             # python poetry related stuff
             unset SOURCE_DATE_EPOCH
             unset LD_PRELOAD
 
+            # Environment variables
+            # fixes libstdc++ issues, libz.so.1 issues
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:${
+              pkgs.lib.makeLibraryPath buildInputs
+            }";
+
             export NODE_OPTIONS="--max-old-space-size=8192"
             echo "increasing node memory allocation to $NODE_OPTIONS"
+
 
 
             if [ -f ./.env ]; then
@@ -165,6 +180,9 @@
 
             export PATH="$(pwd)/node_modules/.bin:$PATH"
 
+            export LD_LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath libraries
+            }:$LD_LIBRARY_PATH
             export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
           '';
           # fixes xcb issues :
