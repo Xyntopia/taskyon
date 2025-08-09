cf5e06a03b4f5296f3cb6dc1531f0e1036b28064
yeus <Thomas.Meschede@web.de>
2025-08-08 22:25:55 -0700

    add a taskyon workspace where we intend to move most of our taskyon files and libraries!
    Signed-off-by: yeus <Thomas.Meschede@web.de>

--- packages/tyclient/src/tyClient.ts
+++ packages/tyclient/src/tyClient.ts
@@ -1,5 +1,6 @@
 // we can compile this file to js to js using "yarn build:lib"
 
+export { createTool } from 'src/modules/taskyon/tools'
 import type { partialTyConfiguration, TaskyonMessage, ClientTool } from './tyClient.types'
 
 function safeClone<T>(data: T): T {
