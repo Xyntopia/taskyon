dc7ba01050e90bdd10010445291cca1ef4de670b
yeus <Thomas.Meschede@web.de>
2025-08-15 11:36:38 -0700

    add taskyon library to yarn v1...

--- packages/tyclient/src/tyClient.types.ts
+++ packages/tyclient/src/tyClient.types.ts
@@ -1,2 +1,2 @@
-export type { partialTyConfiguration, TaskyonMessage } from '../../../src/modules/taskyon/apiTypes'
-export type { ClientTool } from '../../../src/modules/taskyon/tools'
+export type { partialTyConfiguration, TaskyonMessage } from '@taskyon/taskyon'
+export type { ClientTool } from '@taskyon/taskyon'
