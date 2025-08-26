8e8a4659b5591b26a6e4a35a27687b3dd2c817d2
yeus <Thomas.Meschede@web.de>
2025-08-25 23:57:38 -0700

    a little bit of refactoring...

---
+++ src/components/DialogButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-btn v-bind="$attrs" @click.stop="showDialog = true">
+    <slot name="btn"></slot>
+  </q-btn>
+  <q-dialog v-model="showDialog" auto-close>
+    <slot></slot>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const showDialog = ref(false)
+</script>
--- src/components/taskyon/TyResetButton.vue
+++ src/components/taskyon/TyResetButton.vue
@@ -1,76 +1,73 @@
 <template>
-  <q-btn
+  <DialogButton
     :label="
       mode === 'all' ? 'Reset Taskyon' : mode === 'settings' ? 'Reset Settings' : 'Delete Tasks'
     "
     v-bind="$attrs"
-    @click="showResetDialog = true"
   >
-    <q-tooltip :delay="500">
-      {{
-        mode === 'all'
-          ? 'Reset chat history & settings & cache. (Only appears in local development mode)'
-          : mode === 'settings'
-            ? 'Reset settings & cache, but keep tasks'
-            : 'Delete all tasks, but keep settings'
-      }}
-    </q-tooltip>
-    <q-dialog v-model="showResetDialog">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6 text-red text-center">
-            <q-icon :name="matWarning" size="md" />
-            <p v-if="mode === 'settings'">Warning: Reset all Taskyon Settings</p>
-            <p v-else-if="mode === 'tasks'">Warning: Delete Taskyon Chat Data</p>
-            <p v-else>Warning: Completly Wipe out all Taskyon Data</p>
-          </div>
-        </q-card-section>
-        <q-card-section class="q-pt-none">
-          <p v-if="mode === 'tasks'">
-            <strong>Warning:</strong> This operation will permanently delete all Taskyon chat data.
-            This action cannot be undone.
-          </p>
-          <p v-else-if="mode === 'settings'">
-            <strong>Warning:</strong> This operation will reset all taskyon settings. It will *not*
-            delete any of your chats.
-          </p>
-          <p v-else>
-            <strong>Warning:</strong> This operation will permanently delete all taskyon settings
-            including chat data & app settings.
-          </p>
-          <p>
-            Before proceeding, please make sure you have a backup of the settings. You can backup
-            your data here:
-            <q-btn dense flat color="secondary" label="Backup Settings" to="/settings/profile" />
-          </p>
-          <!--<p>
+    <template #btn>
+      <q-tooltip :delay="500">
+        {{
+          mode === 'all'
+            ? 'Reset chat history & settings & cache. (Only appears in local development mode)'
+            : mode === 'settings'
+              ? 'Reset settings & cache, but keep tasks'
+              : 'Delete all tasks, but keep settings'
+        }}
+      </q-tooltip>
+    </template>
+    <q-card>
+      <q-card-section>
+        <div class="text-h6 text-red text-center">
+          <q-icon :name="matWarning" size="md" />
+          <p v-if="mode === 'settings'">Warning: Reset all Taskyon Settings</p>
+          <p v-else-if="mode === 'tasks'">Warning: Delete Taskyon Chat Data</p>
+          <p v-else>Warning: Completly Wipe out all Taskyon Data</p>
+        </div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+        <p v-if="mode === 'tasks'">
+          <strong>Warning:</strong> This operation will permanently delete all Taskyon chat data.
+          This action cannot be undone.
+        </p>
+        <p v-else-if="mode === 'settings'">
+          <strong>Warning:</strong> This operation will reset all taskyon settings. It will *not*
+          delete any of your chats.
+        </p>
+        <p v-else>
+          <strong>Warning:</strong> This operation will permanently delete all taskyon settings
+          including chat data & app settings.
+        </p>
+        <p>
+          Before proceeding, please make sure you have a backup of the settings. You can backup your
+          data here:
+          <q-btn dense flat color="secondary" label="Backup Settings" to="/settings/profile" />
+        </p>
+        <!--<p>
           If you're sure you want to delete all Taskyon chat data, enter "DELETE" in the field below to confirm:
         </p>
         <q-input v-model="deleteConfirmation" label="Confirmation" />-->
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn v-close-popup flat label="Cancel" />
-          <q-btn
-            v-close-popup
-            flat
-            label="Reset"
-            color="negative"
-            :icon="matDeleteForever"
-            @click="onResetTaskyon"
-          />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-  </q-btn>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn v-close-popup flat label="Cancel" />
+        <q-btn
+          v-close-popup
+          flat
+          label="Reset"
+          color="negative"
+          :icon="matDeleteForever"
+          @click="onResetTaskyon"
+        />
+      </q-card-actions>
+    </q-card>
+  </DialogButton>
 </template>
 
 <script setup lang="ts">
 import { matDeleteForever, matWarning } from '@quasar/extras/material-icons'
 import { useAppStateStore } from 'src/stores/appState'
 import { useTaskyonStore } from 'stores/taskyonState'
-import { ref } from 'vue'
-
-const showResetDialog = ref(false)
+import DialogButton from '../DialogButton.vue'
 
 const props = defineProps<{
   mode: 'all' | 'settings' | 'tasks'
