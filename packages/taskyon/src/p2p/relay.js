041d1e4f6ed62b5727783796d98497a0ceedaa40
yeus <Thomas.Meschede@web.de>
2025-09-01 00:24:29 -0700

    add an initial relay server fo forwarding browser connections...

---
+++ packages/taskyon/src/p2p/relay.js
@@ -0,0 +1,24 @@
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
+import { identify } from '@libp2p/identify'
+import { webSockets } from '@libp2p/websockets'
+import { createLibp2p } from 'libp2p'
+
+const node = await createLibp2p({
+  addresses: {
+    listen: ['/ip4/0.0.0.0/tcp/0/ws'],
+    // TODO check "What is next?" section
+  },
+  transports: [webSockets()],
+  connectionEncrypters: [noise()],
+  streamMuxers: [yamux()],
+  services: {
+    identify: identify(),
+    relay: circuitRelayServer(),
+  },
+})
+
+console.log(`Node started with id ${node.peerId.toString()}`)
+console.log('Listening on:')
+node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
