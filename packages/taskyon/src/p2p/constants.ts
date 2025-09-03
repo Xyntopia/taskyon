5755f6501cfcb7021e2903af80a4978ebf4c07ed
yeus <Thomas.Meschede@web.de>
2025-09-02 23:56:51 -0700

    update bootstrap list..

--- packages/taskyon/src/p2p/constants.ts
+++ packages/taskyon/src/p2p/constants.ts
@@ -13,3 +13,16 @@ export const MIME_TEXT_PLAIN = 'text/plain'
 export const WEBTRANSPORT_BOOTSTRAP_PEER_ID = '12D3KooWFhXabKDwALpzqMbto94sB7rvmZ6M28hs9Y9xSopDKwQr'
 
 export const BOOTSTRAP_PEER_IDS = [WEBTRANSPORT_BOOTSTRAP_PEER_ID]
+
+// list taken from here: https://github.com/ipfs/helia/blob/main/packages/helia/src/utils/bootstrappers.ts
+export const bootstrapConfig = {
+  list: [
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+    '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+    // va1 is not in the TXT records for _dnsaddr.bootstrap.libp2p.io yet
+    // so use the host name directly
+    '/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8',
+    '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+  ],
+}
