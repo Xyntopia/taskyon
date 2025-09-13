a828bd6c0439370c66b038a4e45e7057be492df1
yeus <Thomas.Meschede@web.de>
2025-09-13 01:31:08 -0700

    add a component test...

---
+++ src/pages/ComponentTests.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <ResponsiveMenuDialog></ResponsiveMenuDialog>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import ResponsiveMenuDialog from 'src/components/ResponsiveMenuDialog.vue'
+
+//
+</script>
