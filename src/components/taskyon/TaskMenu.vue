c94808dec6f228f465719b9fa49614f018b1a243
yeus <Thomas.Meschede@web.de>
2025-09-13 23:43:32 -0700

    transform task buttons into task menu!

--- src/components/taskyon/TaskMenu.vue
+++ src/components/taskyon/TaskMenu.vue
@@ -1,143 +1,139 @@
 <template>
   <div>
-    <q-btn
-      class="col-auto"
-      :icon="matContentCopy"
-      dense
-      flat
-      size="sm"
-      aria-label="copy markdown text"
-      @click="
-        copyToClipboard(
-          task.content.type === 'message'
-            ? task.content.data || ''
-            : JSON.stringify(task.content.data, null, 2),
-        )
-      "
-    >
-      <q-tooltip :delay="0">
-        Copy {{ task.content.type === 'message' ? 'text (markdown)' : 'JSON' }}.
-      </q-tooltip>
-    </q-btn>
-    <q-btn
-      v-if="task.content.type === 'message' && state.appConfiguration.expertMode"
-      class="col-auto"
-      :icon="
-        state.taskWidgetState[task.id]?.markdownEnabled != false ? mdiLanguageMarkdown : matRawOn
-      "
-      dense
-      flat
-      size="sm"
-      @click="emit('toggle-markdown', task.id)"
-    >
-      <q-tooltip :delay="0">Markdown on/off</q-tooltip>
-    </q-btn>
-    <q-separator vertical class="q-mx-sm" />
-    <q-btn
-      v-if="task.content.type === 'message' || task.content.type === 'functioncall'"
-      class="col-auto"
-      size="sm"
-      dense
-      flat
-      :icon="mdiForumPlus"
-      @click="emit('create-new-conversation', task.id)"
-    >
-      <q-tooltip :delay="0">Start a new thread with this message! </q-tooltip>
-    </q-btn>
-    <q-btn
-      class="col-auto"
-      size="sm"
-      dense
-      flat
-      :icon="matAltRoute"
-      @click="state.setSelectedTask(task.id)"
-    >
-      <q-tooltip :delay="0"
-        >Start a new chat from here, keeping all previous messages up to this point.</q-tooltip
-      >
-    </q-btn>
-    <q-btn
-      v-if="
-        task.content.type === 'message' ||
-        task.content.type === 'functioncall' ||
-        task.content.type === 'tooldefinition'
-      "
-      class="col-auto"
-      :icon="matEdit"
-      dense
-      flat
-      size="sm"
-      @click="emit('edit-task', task.id)"
-    >
-      <q-tooltip :delay="0">
-        Edit
-        {{
-          task.content.type === 'message'
-            ? 'Message'
-            : task.content.type === 'functioncall'
-              ? 'Function Call'
-              : 'Tool Definition'
-        }}
-      </q-tooltip>
-    </q-btn>
-    <q-separator v-if="state.appConfiguration.expertMode" vertical class="q-mx-sm" />
-    <q-btn
-      v-if="state.appConfiguration.expertMode"
-      class="col-auto"
-      :icon="matCode"
-      dense
-      flat
-      size="sm"
-      aria-label="show message context"
-      @click="emit('toggle-message-debug', task.id)"
-    >
-      <q-tooltip :delay="0">Show detailed task view</q-tooltip>
-    </q-btn>
-    <q-btn
-      v-if="state.appConfiguration.expertMode"
-      class="col-auto"
-      :icon="mdiFileTree"
-      dense
-      flat
-      size="sm"
-      aria-label="show message in tree browser"
-      :to="`/browser/${task.id}`"
-    >
-      <q-tooltip :delay="0">Show task tree</q-tooltip>
-    </q-btn>
-    <q-btn
-      v-if="state.appConfiguration.expertMode"
-      class="col-auto"
-      :icon="matSearch"
-      dense
-      flat
-      size="sm"
-      aria-label="show message in tree browser"
-      :to="`/taskmanager?t=${task.id}`"
-    >
-      <q-tooltip :delay="0">Show similar tasks</q-tooltip>
-    </q-btn>
-    <q-btn flat dense size="sm" :icon="matMoreHoriz" @click.prevent.stop>
+    <q-btn flat dense size="sm" :icon="matMoreHoriz">
       <q-menu auto-close>
         <q-list dense>
-          <q-item clickable @click="emit('download', task.id)">
+          <div class="row">
+            <q-btn
+              v-if="
+                task.content.type === 'message' ||
+                task.content.type === 'functioncall' ||
+                task.content.type === 'tooldefinition'
+              "
+              class="col-4 col-sm"
+              :icon="matEdit"
+              dense
+              flat
+              @click="emit('edit-task', task.id)"
+            >
+              <div class="q-px-sm xs">Edit</div>
+              <q-tooltip :delay="0">
+                Edit
+                {{
+                  task.content.type === 'message'
+                    ? 'Message'
+                    : task.content.type === 'functioncall'
+                      ? 'Function Call'
+                      : 'Tool Definition'
+                }}
+              </q-tooltip>
+            </q-btn>
+            <q-btn
+              v-if="task.content.type === 'message' || task.content.type === 'functioncall'"
+              class="col-4 col-sm"
+              dense
+              flat
+              :icon="mdiForumPlus"
+              @click="emit('create-new-conversation', task.id)"
+            >
+              <div class="q-px-sm xs">Start a new chat</div>
+              <q-tooltip :delay="0">Start a new thread with this message! </q-tooltip>
+            </q-btn>
+            <q-btn
+              v-if="state.appConfiguration.expertMode"
+              class="col-4 col-sm"
+              dense
+              flat
+              :icon="matAltRoute"
+              @click="state.setSelectedTask(task.id)"
+            >
+              <div class="q-px-sm xs">Start alternative chat</div>
+              <q-tooltip :delay="0"
+                >Start alternative chat from here, keeping all previous messages up to this
+                point.</q-tooltip
+              >
+            </q-btn>
+            <q-btn
+              class="col"
+              :icon="matContentCopy"
+              dense
+              flat
+              aria-label="copy markdown text"
+              @click="
+                copyToClipboard(
+                  task.content.type === 'message'
+                    ? task.content.data || ''
+                    : JSON.stringify(task.content.data, null, 2),
+                )
+              "
+            >
+              <div class="q-px-sm xs">
+                Copy {{ task.content.type === 'message' ? 'text (markdown)' : 'JSON' }}.
+              </div>
+              <q-tooltip :delay="0">
+                Copy {{ task.content.type === 'message' ? 'text (markdown)' : 'JSON' }}.
+              </q-tooltip>
+            </q-btn>
+          </div>
+          <q-separator />
+          <q-item
+            v-if="state.appConfiguration.expertMode"
+            clickable
+            :to="`/taskmanager?t=${task.id}`"
+          >
             <q-item-section side>
-              <q-icon :name="matDownload"></q-icon>
+              <q-icon :name="matSearch"></q-icon>
             </q-item-section>
-            <q-item-section> Download </q-item-section>
+            <q-item-section> Show similar tasks </q-item-section>
           </q-item>
-          <q-item clickable @click="emit('share', task.id)">
+          <q-item
+            v-if="task.content.type === 'message' && state.appConfiguration.expertMode"
+            clickable
+            @click="emit('toggle-markdown', task.id)"
+          >
             <q-item-section side>
-              <q-icon :name="matShare"></q-icon>
+              <q-icon
+                :name="
+                  state.taskWidgetState[task.id]?.markdownEnabled != false
+                    ? mdiLanguageMarkdown
+                    : matRawOn
+                "
+              ></q-icon>
             </q-item-section>
-            <q-item-section> Share </q-item-section>
+            <q-item-section> Markdown on/off </q-item-section>
           </q-item>
-          <q-item clickable @click="emit('delete', task.id)">
+          <q-separator v-if="state.appConfiguration.expertMode" vertical class="q-mx-sm" />
+          <q-item
+            v-if="state.appConfiguration.expertMode"
+            clickable
+            @click="emit('toggle-message-debug', task.id)"
+          >
             <q-item-section side>
-              <q-icon :name="matDelete"></q-icon>
+              <q-icon :name="matCode"></q-icon>
             </q-item-section>
-            <q-item-section> Delete</q-item-section>
+            <q-item-section> Show detailed task view </q-item-section>
           </q-item>
+          <q-item v-if="state.appConfiguration.expertMode" clickable :to="`/browser/${task.id}`">
+            <q-item-section side>
+              <q-icon :name="mdiFileTree"></q-icon>
+            </q-item-section>
+            <q-item-section> Show task tree </q-item-section>
+          </q-item>
+          <q-separator />
+          <div class="row justify-around" clickable @click="emit('download', task.id)">
+            <q-btn class="col" :icon="matDownload" dense flat @click="emit('download', task.id)">
+              <div class="q-px-sm xs">Download Task</div>
+              <q-tooltip :delay="0"> Download Task </q-tooltip>
+            </q-btn>
+            <q-btn class="col" :icon="matShare" dense flat @click="emit('share', task.id)">
+              <div class="q-px-sm xs">Share Task</div>
+              <q-tooltip :delay="0"> Share Task </q-tooltip>
+            </q-btn>
+            <q-btn class="col" :icon="matDelete" dense flat @click="emit('delete', task.id)">
+              <div class="q-px-sm xs">Delete Task</div>
+              <q-tooltip :delay="0"> Delete Task </q-tooltip>
+            </q-btn>
+          </div>
         </q-list>
       </q-menu>
     </q-btn>
