de0b0c67c964299c9141a66f56d317bdfca40099
yeus <Thomas.Meschede@web.de>
2025-09-16 09:53:46 -0700

    remove lables from first taskmen row...

--- src/components/taskyon/TaskMenu.vue
+++ src/components/taskyon/TaskMenu.vue
@@ -2,18 +2,33 @@
   <q-list>
     <div class="row">
       <q-btn
+        class="col q-pa-sm"
+        :icon="matContentCopy"
+        flat
+        aria-label="copy markdown text"
+        @click="
+          copyToClipboard(
+            task.content.type === 'message'
+              ? task.content.data || ''
+              : JSON.stringify(task.content.data, null, 2),
+          )
+        "
+      >
+        <q-tooltip :delay="0">
+          Copy {{ task.content.type === 'message' ? 'text (markdown)' : 'JSON' }}.
+        </q-tooltip>
+      </q-btn>
+      <q-btn
         v-if="
           task.content.type === 'message' ||
           task.content.type === 'functioncall' ||
           task.content.type === 'tooldefinition'
         "
-        class="col-4 col-sm"
+        class="col"
         :icon="matEdit"
-        dense
         flat
         @click="emit('edit-task', task.id)"
       >
-        <div class="q-px-sm xs">Edit</div>
         <q-tooltip :delay="0">
           Edit
           {{
@@ -27,49 +42,24 @@
       </q-btn>
       <q-btn
         v-if="task.content.type === 'message' || task.content.type === 'functioncall'"
-        class="col-4 col-sm"
-        dense
+        class="col"
         flat
         :icon="mdiForumPlus"
         @click="emit('create-new-conversation', task.id)"
       >
-        <div class="q-px-sm xs">Start a new chat</div>
         <q-tooltip :delay="0">Start a new thread with this message! </q-tooltip>
       </q-btn>
       <q-btn
         v-if="state.appConfiguration.expertMode"
-        class="col-4 col-sm"
-        dense
+        class="col"
         flat
         :icon="matAltRoute"
         @click="state.setSelectedTask(task.id)"
       >
-        <div class="q-px-sm xs">Start alternative chat</div>
         <q-tooltip :delay="0"
           >Start alternative chat from here, keeping all previous messages up to this
           point.</q-tooltip
         >
-      </q-btn>
-      <q-btn
-        class="col"
-        :icon="matContentCopy"
-        dense
-        flat
-        aria-label="copy markdown text"
-        @click="
-          copyToClipboard(
-            task.content.type === 'message'
-              ? task.content.data || ''
-              : JSON.stringify(task.content.data, null, 2),
-          )
-        "
-      >
-        <div class="q-px-sm xs">
-          Copy {{ task.content.type === 'message' ? 'text (markdown)' : 'JSON' }}.
-        </div>
-        <q-tooltip :delay="0">
-          Copy {{ task.content.type === 'message' ? 'text (markdown)' : 'JSON' }}.
-        </q-tooltip>
       </q-btn>
     </div>
     <q-separator />
@@ -114,16 +104,13 @@
     </q-item>
     <q-separator />
     <div class="row justify-around" clickable @click="emit('download', task.id)">
-      <q-btn class="col" :icon="matDownload" dense flat @click="emit('download', task.id)">
-        <div class="q-px-sm xs">Download Task</div>
+      <q-btn class="col q-pa-sm" :icon="matDownload" flat @click="emit('download', task.id)">
         <q-tooltip :delay="0"> Download Task </q-tooltip>
       </q-btn>
-      <q-btn class="col" :icon="matShare" dense flat @click="emit('share', task.id)">
-        <div class="q-px-sm xs">Share Task</div>
+      <q-btn class="col" :icon="matShare" flat @click="emit('share', task.id)">
         <q-tooltip :delay="0"> Share Task </q-tooltip>
       </q-btn>
-      <q-btn class="col" :icon="matDelete" dense flat @click="emit('delete', task.id)">
-        <div class="q-px-sm xs">Delete Task</div>
+      <q-btn class="col" :icon="matDelete" flat @click="emit('delete', task.id)">
         <q-tooltip :delay="0"> Delete Task </q-tooltip>
       </q-btn>
     </div>
