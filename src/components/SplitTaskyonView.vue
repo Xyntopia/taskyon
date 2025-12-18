0f62eefa70e075de37e12a089e48642bb57171ff
yeus <Thomas.Meschede@web.de>
2025-12-17 16:41:32 -0800

    add a split chat view

---
+++ src/components/SplitTaskyonView.vue
@@ -0,0 +1,84 @@
+<template>
+  <DockView v-model:node="layout" class="col" hide-tab-add hide-tab-close>
+    <template #before>
+      <slot name="default"></slot>
+    </template>
+    <template #chat>
+      <!-- Taskyon iframe -->
+      <iframe
+        id="taskyon"
+        title="Taskyon agent"
+        frameborder="0"
+        :src="`${taskyonUrl}?iframe=true&profile=coding`"
+        style="width: 100%; height: 99%"
+      ></iframe>
+    </template>
+  </DockView>
+</template>
+
+<script setup lang="ts">
+import type { DockNode } from 'src/components/DockView.vue'
+import DockView from 'src/components/DockView.vue'
+import type { partialTyConfiguration } from 'src/modules/taskyon/apiTypes'
+import { onMounted, ref } from 'vue'
+import { deepMerge } from '../../packages/taskyon/src/utils/objHelpers'
+import { type ClientTool, initializeTaskyon } from '../../packages/tyclient/src'
+
+const {
+  tools = [],
+  configuration: config = {},
+  persist = false,
+  name,
+} = defineProps<{
+  tools: ClientTool[]
+  configuration: partialTyConfiguration
+  persist?: boolean
+  name: string
+}>()
+
+const taskyonUrl = window.location.origin
+onMounted(() => {
+  const configuration: partialTyConfiguration = deepMerge(
+    {
+      llmSettings: {
+        //selectedApi: 'taskyon',
+        enableOpenAiTools: false,
+        enableToolChooser: true,
+      },
+      appConfiguration: {
+        guiMode: 'minChat',
+        showLogo: false,
+        // TODO: chatSuggestions: [gettingStarted],
+        welcomeMsg: 'Ask taskyon for help with using rumoca/modelica!',
+      },
+      // TODO: signatureOrKey: state.activeTaskyonToken,
+    },
+    config,
+  )
+  void initializeTaskyon({ tools, configuration, name, persist })
+})
+
+const layout = ref<DockNode>({
+  id: 'root',
+  type: 'container',
+  direction: 'row',
+  children: [
+    {
+      id: 'before',
+      type: 'leaf',
+      showTabs: 'never',
+      views: ['before'],
+      size: 30,
+      activeViewIndex: 0,
+    },
+    {
+      id: 'chat',
+      type: 'leaf',
+      showTabs: 'never',
+      views: ['chat'],
+      size: 30,
+      activeViewIndex: 0,
+    },
+  ],
+})
+</script>
