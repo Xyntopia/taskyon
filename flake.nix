7df5a06e82a3ec8ff01c8926391554e2de912e78
yeus <yeusblender@gmail.com>
2026-01-16 18:21:00 -0800

    update flakes! :)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs, nixpkgs_unstable, flake-utils }:
