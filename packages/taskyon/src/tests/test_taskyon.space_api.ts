e464f7c9107a25e2461da5f081a4a7e0eb799cad
yeus <Thomas.Meschede@web.de>
2026-01-30 21:57:33 -0800

    add an upgrade @reclaimprotocol based secure fetch version

--- packages/taskyon/src/tests/test_taskyon.space_api.ts
+++ packages/taskyon/src/tests/test_taskyon.space_api.ts
@@ -1,6 +1,27 @@
 export { testTokenMinting } from '../taskyon.space/taskyon.space_api'
 export { selfTest } from '../experiments/wsProxyFetch'
 
+import { secureFetch } from '@taskyon/secure-tunnel'
+
+export const testSecureFetch = async () => {
+  const response = await secureFetch('https://example.com', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer token123',
+    },
+    tunnelUrl: 'ws://localhost:8443',
+  })
+
+  console.log(response.status)
+  console.log(response.body)
+  const data = await response.json()
+
+  return {
+    data,
+  }
+}
+
+/*
 // 1) Get the wasm file URL as a string (Vite will copy it and give you a URL).
 import httpsTunnelWasmUrl from '../../../https_tunnel_wasm/pkg/https_tunnel_wasm_bg.wasm?url'
 
@@ -24,4 +45,4 @@ export const testWasmHttpsTunne = async () => {
   return {
     success: true,
   }
-}
+}*/
