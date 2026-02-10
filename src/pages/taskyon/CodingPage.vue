73fb24794f19a01cd84d6c05edf10be2af726f33
yeus <Thomas.Meschede@web.de>
2026-02-10 14:19:53 -0800

    another small bug...

--- src/pages/taskyon/CodingPage.vue
+++ src/pages/taskyon/CodingPage.vue
@@ -477,7 +477,7 @@ const tools = [
       additionalProperties: false,
     } as const satisfies JSONSchema7,
     function: (opts) => {
-      const showAllFiles = opts.showAllFiles ?? showAllFilesInPrompt ?? false
+      const showAllFiles = opts.showAllFiles ?? showAllFilesInPrompt.value ?? false
 
       // 1. Context Assembly
       const fileNames = Object.keys(files.value)
