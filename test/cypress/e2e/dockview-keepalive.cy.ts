edd3c1d64f1be982de5a3b3a3fbc1226800e1c67
yeus <yeusblender@gmail.com>
2026-02-22 18:55:26 -0800

    more testing!

--- test/cypress/e2e/dockview-keepalive.cy.ts
+++ test/cypress/e2e/dockview-keepalive.cy.ts
@@ -18,6 +18,21 @@ describe('dockview keepAliveViews', () => {
         cy.wrap(tickCount).as('tickBeforeMinimize')
       })
 
+    // Collapse/restore through the splitter handle (new UX path)
+    cy.dataCy('dock-splitter-toggle-main-0').click()
+    cy.wait(900)
+    cy.dataCy('dock-splitter-toggle-main-0').click()
+    cy.dataCy('dock-view-panel-KeepAliveTicker').should('be.visible')
+    cy.dataCy('dock-view-panel-KeepAliveTicker')
+      .invoke('height')
+      .should('be.greaterThan', 80)
+
+    // Double-click splitter should also toggle collapse/restore
+    cy.dataCy('dock-splitter-toggle-main-0').parent().dblclick()
+    cy.wait(700)
+    cy.dataCy('dock-splitter-toggle-main-0').parent().dblclick()
+
+    // Existing tab-minimize path still works
     cy.dataCy('dock-minimize-panel').click()
     cy.wait(1200)
 
